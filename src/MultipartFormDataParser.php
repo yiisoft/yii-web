7a05aa7c12c38b8d41273847161ce0f932dee72f
MihaOG <miha.o.g@yandex.ru>
2019-01-20 00:46:05 +0300

    Revert "Update MultipartFormDataParser.php"
    This reverts commit 73225b9cafaa2650aacb1aa5ea16cb11b0cd9b46.

--- src/MultipartFormDataParser.php
+++ src/MultipartFormDataParser.php
@@ -192,7 +192,12 @@ class MultipartFormDataParser extends BaseObject implements RequestParserInterfa
                         } else {
                             fwrite($tmpResource, $value);
                             $fileConfig['tempFilename'] = $tmpFileName;
-                            $fileConfig['stream'] = new ResourceStream($tmpResource); // save file resource, otherwise it will be deleted
+                            $fileConfig['stream'] = Yii::createObject([
+                                '__class' => ResourceStream::class,
+                                '__construct()' => [
+                                    'resource' => $tmpResource
+                                ],
+                            ]); // save file resource, otherwise it will be deleted
                         }
                     }
                 }
