1fa0b2805af36adfb4c2d98e3064e65acbde8ebf
Alexander Makarov <sam@rmcreative.ru>
2019-03-18 15:29:28 +0300

    Yes, it makes sense

--- src/router/RouterInterface.php
+++ src/router/RouterInterface.php
@@ -1,7 +1,6 @@
 <?php
 namespace yii\router;
 
-// TODO: is there any benefit having two interfaces except conforming to single responsibility principle?
 interface RouterInterface extends UrlGeneratorInterface, UrlMatcherInterface
 {
 }
