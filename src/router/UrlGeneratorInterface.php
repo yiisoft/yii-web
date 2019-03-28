09bd3bf7a740672ca5acfd15f69c6b85430e819d
Alexander Makarov <sam@rmcreative.ru>
2019-03-28 11:41:48 +0000

    Apply fixes from StyleCI

--- src/router/UrlGeneratorInterface.php
+++ src/router/UrlGeneratorInterface.php
@@ -9,5 +9,5 @@ interface UrlGeneratorInterface
     public const TYPE_ABSOLUTE = 'absolute';
     public const TYPE_RELATIVE = 'relative';
 
-    public function generate(string $name, ServerRequestInterface $request,  array $parameters = [], string $type = self::TYPE_ABSOLUTE): string;
+    public function generate(string $name, ServerRequestInterface $request, array $parameters = [], string $type = self::TYPE_ABSOLUTE): string;
 }
