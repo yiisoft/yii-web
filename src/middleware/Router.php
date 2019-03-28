50c31bc2abf5ef248b6729ffc42bb03522ee5d04
Alexander Makarov <sam@rmcreative.ru>
2019-03-28 18:15:31 +0300

    Fix router middleware

--- src/middleware/Router.php
+++ src/middleware/Router.php
@@ -1,23 +1,25 @@
 <?php
 
 
-namespace yii\middleware;
+namespace yii\web\middleware;
 
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use yii\web\router\NoMatch;
+use yii\web\router\UrlMatcherInterface;
 
 class Router implements MiddlewareInterface
 {
     private $responseFactory;
-    private $routes;
+    private $matcher;
 
-    public function __construct(array $routes, ResponseFactoryInterface $responseFactory)
+    public function __construct(UrlMatcherInterface $matcher, ResponseFactoryInterface $responseFactory)
     {
         $this->responseFactory = $responseFactory;
-        $this->routes = $routes;
+        $this->matcher = $matcher;
     }
 
     /**
@@ -29,24 +31,16 @@ class Router implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        // $result = $this->router->match($request);
-        // if ($result->isSuccess()) {
-        //            return $result->getHandler()->handle($request);
-        //        }
-        // return $handler->handle($request);
 
-        $path = $request->getUri()->getPath();
-
-        if ($path === '/test') {
-            // obtain $handler, process
-
+        try {
+            $match = $this->matcher->match($request);
+            $matchHandler = $match->getHandler();
 
             $response = $this->responseFactory->createResponse(200);
-            $response->getBody()->write($path);
-
-            return $response;
+            // TODO: support returning values and formatting it?
+            return $matchHandler($match->getParameters(), $request, $response);
+        } catch (NoMatch $e) {
+            return $handler->handle($request);
         }
-
-        return $handler->handle($request);
     }
 }
