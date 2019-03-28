5451e5d3023a9137c2419c5801964208dd7a6767
Alexander Makarov <sam@rmcreative.ru>
2019-03-28 15:37:55 +0300

    Pattern matching test

--- tests/router/UrlMatcherTest.php
+++ tests/router/UrlMatcherTest.php
@@ -6,6 +6,7 @@ namespace yii\web\tests\router;
 use Nyholm\Psr7\ServerRequest;
 use PHPUnit\Framework\TestCase;
 use yii\web\router\Group;
+use yii\web\router\Match;
 use yii\web\router\NoHandler;
 use yii\web\router\NoMatch;
 use yii\web\router\Route;
@@ -60,6 +61,7 @@ class UrlMatcherTest extends TestCase
     public function testStaticMatch()
     {
         $handler = function () {
+            // does not matter
         };
         $request = new ServerRequest('GET', '/');
         $matcher = $this->getMatcher([
@@ -70,5 +72,65 @@ class UrlMatcherTest extends TestCase
         $this->assertSame($handler, $match->getHandler());
         $this->assertEmpty($match->getParameters());
         $this->assertNull($match->getName());
+    }
+
+    public function patternMatchProvider()
+    {
+        $handler = function () {
+            // does not matter
+        };
+
+        return [
+            [
+                'book/123/this+is+sample',
+                new Match($handler, ['id' => '123', 'title' => 'this+is+sample'], 'book/view')
+            ],
+            [
+                // trailing slash is significant, no match
+                'book/123/this+is+sample/',
+                null
+            ],
+            [
+                '',
+                null
+            ],
+            [
+                'site/index',
+                null
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider patternMatchProvider
+     */
+    public function testPatternMatch(string $path, ?Match $expectedMatch)
+    {
+        $handler = function () {
+            // does not matter
+        };
+
+        $matcher = $this->getMatcher([
+            Route::get('post/<id:\d+>')
+                ->to($handler)
+                ->name('post/view'),
+            Route::get('posts')
+                ->to($handler)
+                ->name('post/list'),
+            Route::get('book/<id:\d+>/<title>')
+                ->to($handler)
+                ->name('book/view')
+        ]);
+
+        // matching pathinfo
+        $request = new ServerRequest('GET', $path);
+        if ($expectedMatch === null) {
+            $this->expectException(NoMatch::class);
+        }
+
+        $match = $matcher->match($request);
+
+        $this->assertSame($expectedMatch->getName(), $match->getName());
+        $this->assertSame($expectedMatch->getParameters(), $match->getParameters());
     }
 }
