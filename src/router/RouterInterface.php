2c1ac37aa28a91fc12adc8d6bcfed57ff913998f
Alexander Makarov <sam@rmcreative.ru>
2019-05-22 23:47:08 +0300

    Use yiisoft/router

--- src/router/RouterInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace yii\web\router;
-
-interface RouterInterface extends UrlGeneratorInterface, UrlMatcherInterface
-{
-    public function addRoute(Route $route): void;
-}
--- src/router/UrlGeneratorInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace yii\web\router;
-
-interface UrlGeneratorInterface
-{
-    public function generate(string $name, array $parameters = []): string;
-}
