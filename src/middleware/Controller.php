c69b24daa8b92a9ac0b971b4d2844f515ba2a3d2
Alexander Makarov <sam@rmcreative.ru>
2019-06-14 16:46:28 +0300

    Add constructor injection to Controller middleware

--- src/middleware/Controller.php
+++ src/middleware/Controller.php
@@ -1,6 +1,7 @@
 <?php
 namespace Yiisoft\Web\Middleware;
 
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -10,11 +11,13 @@ class Controller implements MiddlewareInterface
 {
     private $class;
     private $method;
+    private $container;
 
-    public function __construct(string $class, string $method)
+    public function __construct(string $class, string $method, ContainerInterface $container)
     {
         $this->class = $class;
         $this->method = $method;
+        $this->container = $container;
     }
 
 
@@ -27,14 +30,13 @@ class Controller implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        // TODO: DI here!
-        $controller = new $this->class();
+        $controller = $this->container->get($this->class);
         // TODO: should we support method injection at all?
         return $controller->{$this->method}($request, $handler);
     }
 
     public static function __set_state(array $properties): self
     {
-        return new self($properties['class'], $properties['method']);
+        return new self($properties['class'], $properties['method'], $properties['container']);
     }
 }
