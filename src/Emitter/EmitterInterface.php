d969ea2f8fe5e4ee42ef3102050f18ac6fb99d2a
Alexander Makarov <sam@rmcreative.ru>
2019-07-05 01:26:23 +0300

    Fix minor phpdoc issues

--- src/Emitter/EmitterInterface.php
+++ src/Emitter/EmitterInterface.php
@@ -5,9 +5,13 @@ namespace Yiisoft\Yii\Web\Emitter;
 use Psr\Http\Message\ResponseInterface;
 
 /**
- * Emitter takes PSR-8 ResponseInterface, formats and outputs it
+ * Emitter takes PSR-7 ResponseInterface, formats and outputs it
  */
 interface EmitterInterface
 {
+    /**
+     * @param ResponseInterface $response
+     * @return bool whether the response have been outputted successfully
+     */
     public function emit(ResponseInterface $response): bool;
 }
