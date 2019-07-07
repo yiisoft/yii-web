a62b3cd4a413c7ad4c125a2de5f71de149b2ad06
Alexander Makarov <sam@rmcreative.ru>
2019-07-07 12:11:38 +0300

    Remove old constants from bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,10 +6,6 @@ use Yiisoft\Di\Container;
 // ensure we get report on all possible php errors
 error_reporting(E_ALL);
 
-define('YII_ENABLE_ERROR_HANDLER', false);
-define('YII_DEBUG', true);
-define('YII_ENV', 'test');
-
 $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
