87f9f3c25a132941761df3c055aa456cd6457dc1
Alexander Makarov <sam@rmcreative.ru>
2019-07-11 00:57:33 +0300

    Added aliases config

--- config/web.php
+++ config/web.php
@@ -1,12 +1,24 @@
 <?php
+use Yiisoft\Aliases\Aliases;
+use Yiisoft\Yii\Web\Application;
+use Yiisoft\Yii\Web\ErrorHandler\HtmlRenderer;
+use Yiisoft\Yii\Web\ErrorHandler\ThrowableRendererInterface;
+
 return [
-    'aliases' => [
+    Aliases::class => [
+        '__class'   => Aliases::class,
+        '@root'     => dirname(__DIR__, 2),
+        '@vendor'   => '@root/vendor',
+        '@public'   => '@root/public',
+        '@runtime'  => '@root/runtime',
+        '@bower'    => '@vendor/bower-asset',
+        '@npm'      => '@vendor/npm-asset',
         '@web' => '/',
     ],
 
     'app' => [
-        '__class' => \Yiisoft\Yii\Web\Application::class,
+        '__class' => Application::class,
     ],
 
-    \Yiisoft\Yii\Web\ErrorHandler\ThrowableRendererInterface::class => \Yiisoft\Yii\Web\ErrorHandler\HtmlRenderer::class,
+    ThrowableRendererInterface::class => HtmlRenderer::class,
 ];
