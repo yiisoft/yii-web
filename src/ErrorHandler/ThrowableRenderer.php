b098657d1ddbd5b906c798c569d9263dd3ef7023
Alexander Makarov <sam@rmcreative.ru>
2019-07-10 13:19:43 +0300

    Add missing space

--- src/ErrorHandler/ThrowableRenderer.php
+++ src/ErrorHandler/ThrowableRenderer.php
@@ -41,7 +41,7 @@ abstract class ThrowableRenderer implements ThrowableRendererInterface
 
     protected function convertThrowableToVerboseString(\Throwable $t): string
     {
-        $message = $this->getThrowableName($t) . "with message '{$t->getMessage()}' \n\nin "
+        $message = $this->getThrowableName($t) . " with message '{$t->getMessage()}' \n\nin "
             . $t->getFile() . ':' . $t->getLine() . "\n\n"
             . "Stack trace:\n" . $t->getTraceAsString();
         return $message;
