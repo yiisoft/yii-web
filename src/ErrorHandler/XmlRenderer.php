b64b15c5d660689a4ea598bee534ab00d54c6619
Alexander Makarov <sam@rmcreative.ru>
2019-07-10 22:29:14 +0300

    Fix XML error renderer

--- src/ErrorHandler/XmlRenderer.php
+++ src/ErrorHandler/XmlRenderer.php
@@ -4,7 +4,7 @@ namespace Yiisoft\Yii\Web\ErrorHandler;
 /**
  * Formats exception into XML string
  */
-class XmlRenderer implements ThrowableRendererInterface
+class XmlRenderer extends ThrowableRenderer
 {
     public function render(\Throwable $t): string
     {
