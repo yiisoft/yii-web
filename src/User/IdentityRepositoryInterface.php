14fc03c37e304e7e5860d3bf1bc5aeb55013d7f6
Alexander Makarov <sam@rmcreative.ru>
2019-08-13 01:15:38 +0300

    Fix interface types

--- src/User/IdentityRepositoryInterface.php
+++ src/User/IdentityRepositoryInterface.php
@@ -3,16 +3,16 @@ namespace Yiisoft\Yii\Web\User;
 
 interface IdentityRepositoryInterface
 {
-    public function findIdentity($id): self;
+    public function findIdentity(string $id): ?IdentityInterface;
 
     /**
      * Finds an identity by the given token.
-     * @param mixed $token the token to be looked for
-     * @param mixed $type the type of the token. The value of this parameter depends on the implementation and should
+     * @param string $token the token to be looked for
+     * @param string $type the type of the token. The value of this parameter depends on the implementation and should
      * allow supporting multiple token types for a single identity.
      * @return IdentityInterface|null the identity object that matches the given token.
      * Null should be returned if such an identity cannot be found
      * or the identity is not in an active state (disabled, deleted, etc.)
      */
-    public function findIdentityByToken(string $token, string $type): ?self;
+    public function findIdentityByToken(string $token, string $type): ?IdentityInterface;
 }
