d7df4fd8672494b9227443dc88fbded4a444a404
Alexander Makarov <sam@rmcreative.ru>
2019-09-18 13:34:17 +0300

    Ported getting trace for fatals in case of XDebug is installed

--- src/ErrorHandler/ThrowableRenderer.php
+++ src/ErrorHandler/ThrowableRenderer.php
@@ -5,24 +5,6 @@ use Psr\Http\Message\ServerRequestInterface;
 
 abstract class ThrowableRenderer implements ThrowableRendererInterface
 {
-    private const SEVERITY_NAMES = [
-        E_COMPILE_ERROR => 'PHP Compile Error',
-        E_COMPILE_WARNING => 'PHP Compile Warning',
-        E_CORE_ERROR => 'PHP Core Error',
-        E_CORE_WARNING => 'PHP Core Warning',
-        E_DEPRECATED => 'PHP Deprecated Warning',
-        E_ERROR => 'PHP Fatal Error',
-        E_NOTICE => 'PHP Notice',
-        E_PARSE => 'PHP Parse Error',
-        E_RECOVERABLE_ERROR => 'PHP Recoverable Error',
-        E_STRICT => 'PHP Strict Warning',
-        E_USER_DEPRECATED => 'PHP User Deprecated Warning',
-        E_USER_ERROR => 'PHP User Error',
-        E_USER_NOTICE => 'PHP User Notice',
-        E_USER_WARNING => 'PHP User Warning',
-        E_WARNING => 'PHP Warning',
-    ];
-
     /**
      * @var ServerRequestInterface
      */
@@ -32,8 +14,8 @@ abstract class ThrowableRenderer implements ThrowableRendererInterface
     {
         $name = get_class($t);
 
-        if ($t instanceof \ErrorException && isset(self::SEVERITY_NAMES[$t->getSeverity()])) {
-            $name .= ' (' . self::SEVERITY_NAMES[$t->getSeverity()] . ')';
+        if ($t instanceof FriendlyExceptionInterface) {
+            $name = $t->getName() . ' (' . $name . ')';
         }
 
         return $name;
