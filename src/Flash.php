6e9bd6f9dcc9237aa110a6d77fae43948ec62a8d
Pavel Chaplygin <pavel@spacepass.pro>
2019-09-22 00:34:37 +0700

    Implement flash messages (#100) with separate namespace in $_SESSION (#31).
    Logic was ported from Yii 2 with the exception of separate namespace for flashes.

---
+++ src/Flash.php
@@ -0,0 +1,231 @@
+<?php
+
+namespace Yiisoft\Yii\Web;
+
+use Yiisoft\Yii\Web\Session\SessionInterface;
+
+class Flash
+{
+    private const COUNTERS = '__counters';
+
+    /**
+     * @var string the name of the session variable that stores the flash message data.
+     */
+    public $flashParam = '__flash';
+
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    private static $init;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+
+        // todo: not sure if it's ok
+        $this->session->open();
+        if (self::$init !== $this->session->getId()) {
+            self::$init = $this->session->getId();
+            $this->updateCounters();
+        }
+    }
+
+    /**
+     * Returns a flash message.
+     * @param string $key the key identifying the flash message
+     * @param mixed $defaultValue value to be returned if the flash message does not exist.
+     * @param bool $delete whether to delete this flash message right after this method is called.
+     * If false, the flash message will be automatically deleted in the next request.
+     * @return mixed the flash message or an array of messages if addFlash was used
+     */
+    public function getFlash(string $key, $defaultValue = null, bool $delete = false)
+    {
+        $flashes = $this->fetch();
+
+        if (isset($flashes[$key], $flashes[self::COUNTERS][$key])) {
+            $value = $flashes[$key];
+
+            if ($delete) {
+                $this->removeFlash($key);
+            } elseif ($flashes[self::COUNTERS][$key] < 0) {
+                // mark for deletion in the next request
+                $flashes[self::COUNTERS][$key] = 1;
+                $this->save($flashes);
+            }
+
+            return $value;
+        }
+
+        return $defaultValue;
+    }
+
+    /**
+     * Returns all flash messages.
+     *
+     * You may use this method to display all the flash messages in a view file:
+     *
+     * ```php
+     * <?php
+     * foreach ($flash->getAllFlashes() as $key => $message) {
+     *     echo '<div class="alert alert-' . $key . '">' . $message . '</div>';
+     * } ?>
+     * ```
+     *
+     * With the above code you can use the [bootstrap alert][] classes such as `success`, `info`, `danger`
+     * as the flash message key to influence the color of the div.
+     *
+     * Note that if you use [[addFlash()]], `$message` will be an array, and you will have to adjust the above code.
+     *
+     * [bootstrap alert]: http://getbootstrap.com/components/#alerts
+     *
+     * @param bool $delete whether to delete the flash messages right after this method is called.
+     * If false, the flash messages will be automatically deleted in the next request.
+     * @return array flash messages (key => message or key => [message1, message2]).
+     */
+    public function getAllFlashes(bool $delete = false): array
+    {
+        $flashes = $this->fetch();
+
+        $list = [];
+        foreach ($flashes as $key => $value) {
+            if ($key === self::COUNTERS) {
+                continue;
+            }
+
+            $list[$key] = $value;
+            if ($delete) {
+                unset($flashes[self::COUNTERS][$key], $flashes[$key]);
+            } elseif ($flashes[self::COUNTERS][$key] < 0) {
+                // mark for deletion in the next request
+                $flashes[self::COUNTERS][$key] = 1;
+            }
+        }
+
+        $this->save($flashes);
+
+        return $list;
+    }
+
+    /**
+     * Sets a flash message.
+     * A flash message will be automatically deleted after it is accessed in a request and the deletion will happen
+     * in the next request.
+     * If there is already an existing flash message with the same key, it will be overwritten by the new one.
+     * @param string $key the key identifying the flash message.
+     * @param mixed $value flash message
+     * @param bool $removeAfterAccess whether the flash message should be automatically removed only if
+     * it is accessed. If false, the flash message will be automatically removed after the next request,
+     * regardless if it is accessed or not. If true (default value), the flash message will remain until after
+     * it is accessed.
+     */
+    public function setFlash(string $key, $value = true, bool $removeAfterAccess = true): void
+    {
+        $flashes = $this->fetch();
+        $flashes[self::COUNTERS][$key] = $removeAfterAccess ? -1 : 0;
+        $flashes[$key] = $value;
+        $this->save($flashes);
+    }
+
+    /**
+     * Adds a flash message.
+     * If there are existing flash messages with the same key, the new one will be appended to the existing message array.
+     * @param string $key the key identifying the flash message.
+     * @param mixed $value flash message
+     * @param bool $removeAfterAccess whether the flash message should be automatically removed only if
+     * it is accessed. If false, the flash message will be automatically removed after the next request,
+     * regardless if it is accessed or not. If true (default value), the flash message will remain until after
+     * it is accessed.
+     */
+    public function addFlash(string $key, $value = true, bool $removeAfterAccess = true): void
+    {
+        $flashes = $this->fetch();
+        $flashes[self::COUNTERS][$key] = $removeAfterAccess ? -1 : 0;
+
+        if (empty($flashes[$key])) {
+            $flashes[$key] = [$value];
+        } elseif (is_array($flashes[$key])) {
+            $flashes[$key][] = $value;
+        } else {
+            $flashes[$key] = [$flashes[$key], $value];
+        }
+
+        $this->save($flashes);
+    }
+
+    /**
+     * Removes a flash message.
+     * @param string $key the key identifying the flash message.
+     * @return mixed the removed flash message. Null if the flash message does not exist.
+     */
+    public function removeFlash(string $key)
+    {
+        $flashes = $this->fetch();
+
+        $value = isset($flashes[$key], $flashes[self::COUNTERS][$key]) ? $flashes[$key] : null;
+        unset($flashes[$key], $flashes[self::COUNTERS][$key]);
+
+        $this->save($flashes);
+
+        return $value;
+    }
+
+    /**
+     * Removes all flash messages.
+     */
+    public function removeAllFlashes(): void
+    {
+        $this->save([self::COUNTERS => []]);
+    }
+
+    /**
+     * Returns a value indicating whether there are flash messages associated with the specified key.
+     * @param string $key key identifying the flash message type
+     * @return bool whether any flash messages exist under specified key
+     */
+    public function hasFlash(string $key): bool
+    {
+        $flashes = $this->fetch();
+        return isset($flashes[$key], $flashes[self::COUNTERS][$key]);
+    }
+
+    /**
+     * Updates the counters for flash messages and removes outdated flash messages.
+     * This method should be called once after session initialization.
+     */
+    private function updateCounters(): void
+    {
+        $flashes = $this->session->get($this->flashParam, []);
+        if (!is_array($flashes)) {
+            $flashes = [self::COUNTERS => []];
+        }
+
+        $counters = $flashes[self::COUNTERS] ?? [];
+        if (!is_array($counters)) {
+            $counters = [];
+        }
+
+
+        foreach ($counters as $key => $count) {
+            if ($count > 0) {
+                unset($counters[$key], $flashes[$key]);
+            } elseif ($count === 0) {
+                $counters[$key]++;
+            }
+        }
+
+        $flashes[self::COUNTERS] = $counters;
+        $this->save($flashes);
+    }
+
+    private function fetch(): array
+    {
+        return $this->session->get($this->flashParam, []);
+    }
+
+    private function save(array $flashes): void
+    {
+        $this->session->set($this->flashParam, $flashes);
+    }
+}
