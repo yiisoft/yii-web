738d770288d68574dc151de529d7fb5603d18dcd
Pavel Chaplygin <pavel@spacepass.pro>
2019-09-22 21:05:27 +0700

    Extract FlashInterface

--- src/Flash.php
+++ src/Flash.php
@@ -4,7 +4,7 @@ namespace Yiisoft\Yii\Web;
 
 use Yiisoft\Yii\Web\Session\SessionInterface;
 
-final class Flash
+final class Flash implements FlashInterface
 {
     private const COUNTERS = '__counters';
 
@@ -20,14 +20,6 @@ final class Flash
         $this->session = $session;
     }
 
-    /**
-     * Returns a flash message.
-     * @param string $key the key identifying the flash message
-     * @param mixed $defaultValue value to be returned if the flash message does not exist.
-     * @param bool $delete whether to delete this flash message right after this method is called.
-     * If false, the flash message will be automatically deleted in the next request.
-     * @return mixed the flash message or an array of messages if addFlash was used
-     */
     public function get(string $key, $defaultValue = null, bool $delete = false)
     {
         $flashes = $this->fetch();
@@ -49,29 +41,6 @@ final class Flash
         return $defaultValue;
     }
 
-    /**
-     * Returns all flash messages.
-     *
-     * You may use this method to display all the flash messages in a view file:
-     *
-     * ```php
-     * <?php
-     * foreach ($flash->getAllFlashes() as $key => $message) {
-     *     echo '<div class="alert alert-' . $key . '">' . $message . '</div>';
-     * } ?>
-     * ```
-     *
-     * With the above code you can use the [bootstrap alert][] classes such as `success`, `info`, `danger`
-     * as the flash message key to influence the color of the div.
-     *
-     * Note that if you use [[addFlash()]], `$message` will be an array, and you will have to adjust the above code.
-     *
-     * [bootstrap alert]: http://getbootstrap.com/components/#alerts
-     *
-     * @param bool $delete whether to delete the flash messages right after this method is called.
-     * If false, the flash messages will be automatically deleted in the next request.
-     * @return array flash messages (key => message or key => [message1, message2]).
-     */
     public function getAll(bool $delete = false): array
     {
         $flashes = $this->fetch();
@@ -96,18 +65,6 @@ final class Flash
         return $list;
     }
 
-    /**
-     * Sets a flash message.
-     * A flash message will be automatically deleted after it is accessed in a request and the deletion will happen
-     * in the next request.
-     * If there is already an existing flash message with the same key, it will be overwritten by the new one.
-     * @param string $key the key identifying the flash message.
-     * @param mixed $value flash message
-     * @param bool $removeAfterAccess whether the flash message should be automatically removed only if
-     * it is accessed. If false, the flash message will be automatically removed after the next request,
-     * regardless if it is accessed or not. If true (default value), the flash message will remain until after
-     * it is accessed.
-     */
     public function set(string $key, $value = true, bool $removeAfterAccess = true): void
     {
         $flashes = $this->fetch();
@@ -116,16 +73,6 @@ final class Flash
         $this->save($flashes);
     }
 
-    /**
-     * Adds a flash message.
-     * If there are existing flash messages with the same key, the new one will be appended to the existing message array.
-     * @param string $key the key identifying the flash message.
-     * @param mixed $value flash message
-     * @param bool $removeAfterAccess whether the flash message should be automatically removed only if
-     * it is accessed. If false, the flash message will be automatically removed after the next request,
-     * regardless if it is accessed or not. If true (default value), the flash message will remain until after
-     * it is accessed.
-     */
     public function add(string $key, $value = true, bool $removeAfterAccess = true): void
     {
         $flashes = $this->fetch();
@@ -142,11 +89,6 @@ final class Flash
         $this->save($flashes);
     }
 
-    /**
-     * Removes a flash message.
-     * @param string $key the key identifying the flash message.
-     * @return mixed the removed flash message. Null if the flash message does not exist.
-     */
     public function remove(string $key)
     {
         $flashes = $this->fetch();
@@ -159,19 +101,11 @@ final class Flash
         return $value;
     }
 
-    /**
-     * Removes all flash messages.
-     */
     public function removeAll(): void
     {
         $this->save([self::COUNTERS => []]);
     }
 
-    /**
-     * Returns a value indicating whether there are flash messages associated with the specified key.
-     * @param string $key key identifying the flash message type
-     * @return bool whether any flash messages exist under specified key
-     */
     public function has(string $key): bool
     {
         $flashes = $this->fetch();
