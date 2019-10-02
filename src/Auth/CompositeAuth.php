6a90dec106330e1f552413908f35b89a99276528
Rustam Mamadaminov <rmamdaminov@gmail.com>
2019-10-02 14:56:54 +0500

    Implement authentication middleware (#116)

---
+++ src/Auth/CompositeAuth.php
@@ -0,0 +1,62 @@
+<?php
+namespace Yiisoft\Yii\Web\Auth;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Yiisoft\Yii\Web\User\IdentityInterface;
+
+/**
+ * CompositeAuth allows multiple authentication methods at the same time.
+ *
+ * The authentication methods contained by CompositeAuth are configured via {@see setAuthMethods()},
+ * which is a list of supported authentication class configurations.
+ */
+final class CompositeAuth implements AuthInterface
+{
+    /**
+     * @var AuthInterface[]
+     */
+    private $authMethods = [];
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function authenticate(ServerRequestInterface $request): ?IdentityInterface
+    {
+        foreach ($this->authMethods as $i => $auth) {
+            if (!$auth instanceof AuthInterface) {
+                $this->authMethods[$i] = $auth = $this->container->get($auth);
+                if (!$auth instanceof AuthInterface) {
+                    throw new \RuntimeException(get_class($auth) . ' must implement ' . AuthInterface::class);
+                }
+            }
+
+            $identity = $auth->authenticate($request);
+            if ($identity !== null) {
+                return $identity;
+            }
+        }
+
+        return null;
+    }
+
+    public function challenge(ResponseInterface $response): ResponseInterface
+    {
+        foreach ($this->authMethods as $method) {
+            $response = $method->challenge($response);
+        }
+        return $response;
+    }
+
+    public function setAuthMethods(array $methods): void
+    {
+        $this->authMethods = $methods;
+    }
+}
---
+++ src/Auth/HttpBasicAuth.php
@@ -0,0 +1,112 @@
+<?php
+namespace Yiisoft\Yii\Web\Auth;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Yiisoft\Yii\Web\User\IdentityInterface;
+use Yiisoft\Yii\Web\User\IdentityRepositoryInterface;
+
+/**
+ * HttpBasicAuth supports the HTTP Basic authentication method.
+ *
+ * > Tip: In case authentication does not work like expected, make sure your web server passes
+ * username and password to `$request->getServerParams()['PHP_AUTH_USER']` and `$request->getServerParams()['PHP_AUTH_PW']` variables.
+ * If you are using Apache with PHP-CGI, you might need to add this line to your `.htaccess` file:
+ * ```
+ * RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization},L]
+ * ```
+ */
+final class HttpBasicAuth implements AuthInterface
+{
+    /**
+     * @var string the HTTP authentication realm
+     */
+    private $realm = 'api';
+    /**
+     * @var callable a PHP callable that will authenticate the user with the HTTP basic auth information.
+     * The callable receives a username and a password as its parameters. It should return an identity object
+     * that matches the username and password. Null should be returned if there is no such identity.
+     * The callable will be called only if current user is not authenticated.
+     *
+     * If this property is not set, the username information will be considered as an access token
+     * while the password information will be ignored. The {@see \Yiisoft\Yii\Web\User\IdentityRepositoryInterface::findIdentityByToken()}
+     * method will be called to authenticate and login the user.
+     */
+    private $auth;
+    /**
+     * @var IdentityRepositoryInterface
+     */
+    private $identityRepository;
+
+    public function __construct(IdentityRepositoryInterface $identityRepository)
+    {
+        $this->identityRepository = $identityRepository;
+    }
+
+    public function authenticate(ServerRequestInterface $request): ?IdentityInterface
+    {
+        [$username, $password] = $this->getAuthCredentials($request);
+
+        if ($this->auth) {
+            if ($username !== null || $password !== null) {
+                $identity = \call_user_func($this->auth, $username, $password);
+
+                return $identity;
+            }
+        } elseif ($username !== null) {
+            $identity = $this->identityRepository->findIdentityByToken($username, get_class($this));
+
+            return $identity;
+        }
+
+        return null;
+    }
+
+    public function challenge(ResponseInterface $response): ResponseInterface
+    {
+        return $response->withHeader('WWW-Authenticate', "Basic realm=\"{$this->realm}\"");
+    }
+
+    public function setAuth(callable $auth): void
+    {
+        $this->auth = $auth;
+    }
+
+    public function setRealm(string $realm): void
+    {
+        $this->realm = $realm;
+    }
+
+    private function getAuthCredentials(ServerRequestInterface $request): array
+    {
+        $username = $request->getServerParams()['PHP_AUTH_USER'] ?? null;
+        $password = $request->getServerParams()['PHP_AUTH_PW'] ?? null;
+        if ($username !== null || $password !== null) {
+            return [$username, $password];
+        }
+
+        /*
+         * Apache with php-cgi does not pass HTTP Basic authentication to PHP by default.
+         * To make it work, add the following line to to your .htaccess file:
+         *
+         * RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+         */
+        $headers = $request->getHeader('Authorization');
+        $authToken = !empty($headers)
+            ? \reset($headers)
+            : $request->getServerParams()['REDIRECT_HTTP_AUTHORIZATION'] ?? null;
+        if ($authToken !== null && strncasecmp($authToken, 'basic', 5) === 0) {
+            $parts = array_map(static function ($value) {
+                return strlen($value) === 0 ? null : $value;
+            }, explode(':', base64_decode(mb_substr($authToken, 6)), 2));
+
+            if (\count($parts) < 2) {
+                return [$parts[0], null];
+            }
+
+            return $parts;
+        }
+
+        return [null, null];
+    }
+}
