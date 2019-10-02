3c79d60726bfa6ded7d086d98b0d9bcecec8ed8b
Rustam Mamadaminov <rmamdaminov@gmail.com>
2019-10-02 14:56:54 +0500

    Implement authentication middleware (#116)

---
+++ src/Auth/HttpBearerAuth.php
@@ -0,0 +1,30 @@
+<?php
+namespace Yiisoft\Yii\Web\Auth;
+
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * HttpBearerAuth supports the authentication method based on HTTP Bearer token.
+ */
+final class HttpBearerAuth extends HttpHeaderAuth
+{
+    private const HEADER_NAME = 'Authorization';
+    private const PATTERN = '/^Bearer\s+(.*?)$/';
+
+    protected $headerName = self::HEADER_NAME;
+    protected $pattern = self::PATTERN;
+    /**
+     * @var string the HTTP authentication realm
+     */
+    private $realm = 'api';
+
+    public function challenge(ResponseInterface $response): ResponseInterface
+    {
+        return $response->withHeader('WWW-Authenticate', "{$this->headerName} realm=\"{$this->realm}\"");
+    }
+
+    public function setRealm(string $realm): void
+    {
+        $this->realm = $realm;
+    }
+}
