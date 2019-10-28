8be3a6e38b00773cff0bc4dda7f9b239bd037473
Somogyi Márton <kiss.albert.marton@wwdh.hu>
2019-10-28 12:12:12 +0100

    TrustedHostsNetworkResolver snapshot #119

--- src/Middleware/TrustedHostsNetworkResolver.php
+++ src/Middleware/TrustedHostsNetworkResolver.php
@@ -4,8 +4,11 @@
 namespace Yiisoft\Yii\Web\Middleware;
 
 
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use Yiisoft\Validator\Rule\Ip;
 
 class TrustedHostsNetworkResolver implements MiddlewareInterface
@@ -34,29 +37,52 @@ class TrustedHostsNetworkResolver implements MiddlewareInterface
     private $trustedHosts = [];
 
     /**
-     * @var ServerRequestInterface|null
+     * @var string|null
      */
-    private $baseServerRequest;
+    private $attributeIps = null;
 
     /**
-     * @var int
+     * @var ResponseFactoryInterface
      */
-    private $remoteIpIndex = 0;
+    private $responseFactory;
+    /**
+     * @var Chain|null
+     */
+    private $notTrustedBranch;
 
     /**
-     * @var array
+     * @var Ip|null
      */
-    private $cacheIpList = [];
-    private $cacheIsTrusted = false;
-    /**
-     * @var ServerRequestInterface|null
-     */
-    private $cacheServerRequest;
+    private $ipValidator;
+
+    public function __construct(ResponseFactoryInterface $responseFactory)
+    {
+        $this->responseFactory = $responseFactory;
+    }
 
     /**
      * @return static
      */
-    public function withNewTrustedHosts(
+    public function withIpValidator(Ip $ipValidator) {
+        $new = clone $this;
+        $new->ipValidator = $ipValidator;
+        return $new;
+    }
+
+    /**
+     * @return static
+     */
+    public function withNotTrustedBranch(?MiddlewareInterface $middleware)
+    {
+        $new = clone $this;
+        $new->notTrustedBranch = $middleware;
+        return $new;
+    }
+
+    /**
+     * @return static
+     */
+    public function withAddedTrustedHosts(
         array $hosts,
         ?array $ipHeaders = null,
         ?array $protocolHeaders = null,
@@ -82,123 +108,46 @@ class TrustedHostsNetworkResolver implements MiddlewareInterface
         return $new;
     }
 
+    /**
+     * @return static
+     */
+    public function withAttributeIps(?string $attribute)
+    {
+        if ($attribute !== null && strlen($attribute) === 0) {
+            throw new \RuntimeException('Attribute is cannot be an empty string!');
+        }
+        $new = clone $this;
+        $new->attributeIps = $attribute;
+        return $new;
+    }
+
     protected function prepareProtocolHeaders(array $protocolHeaders): array
     {
         $output = [];
         foreach ($protocolHeaders as $header => $protocolAndAcceptedValues) {
+            $header = strtolower($header);
             if (is_callable($protocolAndAcceptedValues)) {
                 $output[$header] = $protocolAndAcceptedValues;
-            } elseif (!is_array($protocolAndAcceptedValues)) {
-                throw new \RuntimeException('$protocolAndAcceptedValues is not array nor callable!');
-            } elseif (is_array($protocolAndAcceptedValues) && count($protocolAndAcceptedValues) === 0) {
-                throw new \RuntimeException('$protocolAndAcceptedValues cannot be an empty array!');
-            } else {
-                $output[$header] = [];
-                foreach ($protocolAndAcceptedValues as $protocol => $acceptedValues) {
-                    if (!is_string($protocol)) {
-                        throw new \RuntimeException('The protocol must be type of string!');
-                    }
-                    $output[$header][$protocol] = array_map('strtolower', (array)$acceptedValues);
+                continue;
+            }
+            if (!is_array($protocolAndAcceptedValues)) {
+                throw new \RuntimeException('Accepted values is not array nor callable!');
+            }
+            if (count($protocolAndAcceptedValues) === 0) {
+                throw new \RuntimeException('Accepted values cannot be an empty array!');
+            }
+            $output[$header] = [];
+            foreach ($protocolAndAcceptedValues as $protocol => $acceptedValues) {
+                if (!is_string($protocol)) {
+                    throw new \RuntimeException('The protocol must be type of string!');
                 }
+                if (strlen($protocol) === 0) {
+                    throw new \RuntimeException('The protocol cannot be an empty string!');
+                }
+                $output[$header][$protocol] = array_map('strtolower', (array)$acceptedValues);
             }
         }
         return $output;
-    }
-
-    /**
-     * @return static
-     */
-    public function withServerRequest(ServerRequestInterface $serverRequest)
-    {
-        $new = clone $this;
-        $new->baseServerRequest = $serverRequest;
-        return $new;
-    }
-
-    public function getRemoteIp(): string
-    {
-        $this->getServerRequest();
-        return $this->cacheIpList[0];
-    }
-
-    public function getUserIp(): string
-    {
-        $this->getServerRequest();
-        return end($this->cacheIpList);
-    }
-
-    /**
-     * Security of user's connection
-     */
-    public function isSecureConnection(): bool
-    {
-        return $this->getServerRequest()->getUri()->getScheme() === 'https';
-    }
-
-    public function __clone()
-    {
-        $this->cacheIpList = [];
-        $this->cacheIsTrusted = false;
-        $this->cacheServerRequest = null;
-    }
-
-    protected function getBaseServerRequest(bool $throwIfNull = true): ?ServerRequestInterface
-    {
-        if ($this->baseServerRequest === null && $throwIfNull) {
-            throw new \RuntimeException('The server request object is not set!');
-        }
-        return $this->baseServerRequest;
-    }
-
-    public function getServerRequest(): ServerRequestInterface
-    {
-        if ($this->cacheServerRequest !== null) {
-            return $this->cacheServerRequest;
-        }
-
-        $request = $this->getBaseServerRequest();
-        $actualHost = $request->getServerParams()['REMOTE_ADDR'];
-        $this->cacheIpList = [$actualHost];
-        $trustedHostData = null;
-        $trustedHeadersMerge = [];
-        foreach ($this->trustedHosts as $data) {
-            $trustedHeadersMerge = array_merge($trustedHeadersMerge, $data['trustedHeaders']);
-            if ($trustedHostData !== null) {
-                continue;
-            } elseif (!$this->isValidHost($actualHost, $data['hosts'])) {
-                continue;
-            }
-            $trustedHostData = $data;
-        }
-        if ($trustedHostData === null) {
-            // No trusted host at all.
-            return $this->cacheServerRequest = $this->removeHeaders($request, $trustedHeadersMerge);
-        }
-
-        $request = $this->removeHeaders($request, array_diff($trustedHeadersMerge, $trustedHostData['trustedHeaders']));
-
-        $ipList = null;
-        foreach ($trustedHostData['ipHeaders'] as $ipHeader) {
-            if ($request->hasHeader($ipHeader)) {
-                $ipList = $request->getHeader($ipHeader)[0];
-                break;
-            }
-        }
-
-        if ($ipList !== null) {
-            $ips = preg_split('/\s*,\s*/', trim($ipList), -1, PREG_SPLIT_NO_EMPTY);
-            while (count($ips)) {
-                $ip = array_pop($ips);
-                if($this->isValidHost($ip, ['any'])) {
-                    $this->cacheIpList[] = $ip;
-                }
-                if (!$this->isValidHost($ip, $trustedHostData['hosts'])) {
-                    break;
-                }
-            }
-        }
-
-        return $this->cacheServerRequest = $request;
     }
 
     protected function removeHeaders(ServerRequestInterface $request, array $headers): ServerRequestInterface
@@ -213,19 +162,79 @@ class TrustedHostsNetworkResolver implements MiddlewareInterface
      * Validate host by range
      *
      * This method can be extendable by overwriting eg. with reverse DNS verification.
-     *
-     * @param string   $host
-     * @param string[] $ranges
      */
-    protected function isValidHost(string $host, array $ranges): bool
+    protected function isValidHost(string $host, array $ranges, Ip $validator): bool
     {
-// @TODO Ip validator not working
-//        if($ranges == ['any']) {
-//            return true;
-//        }
-//        return $host == $ranges[0];
-        $validator = new Ip();
         $validator->setRanges($ranges);
-        return $validator->validateValue($host)->isValid();
+        return $validator->validate($host)->isValid();
+    }
+
+    /**
+     * Process an incoming server request.
+     *
+     * Processes an incoming server request in order to produce a response.
+     * If unable to produce the response itself, it may delegate to the provided
+     * request handler to do so.
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $actualHost = $request->getServerParams()['REMOTE_ADDR'];
+        $ips = [$actualHost];
+        $trustedHostData = null;
+        $trustedHeaders = [];
+        $ipValidator = $this->ipValidator ?? new Ip();
+        foreach ($this->trustedHosts as $data) {
+            $trustedHeaders = array_merge($trustedHeaders, $data['trustedHeaders']);
+            if ($trustedHostData !== null) {
+                continue;
+            }
+            if (!$this->isValidHost($actualHost, $data['hosts'], $ipValidator)) {
+                continue;
+            }
+            $trustedHostData = $data;
+        }
+        $untrustedHeaders = array_diff($trustedHeaders, $trustedHostData['trustedHeaders'] ?? []);
+        $request = $this->removeHeaders($request, $untrustedHeaders);
+        if ($trustedHostData === null) {
+            // No trusted host at all.
+            if ($this->notTrustedBranch !== null) {
+                return $this->notTrustedBranch->process($request, $handler);
+            }
+            $response = $this->responseFactory->createResponse(412);
+            $response->getBody()->write('Unable to verify your network.');
+            return $response;
+        }
+
+        $ipList = null;
+        foreach ($trustedHostData['ipHeaders'] as $ipHeader) {
+            if ($request->hasHeader($ipHeader)) {
+                $ipList = $request->getHeader($ipHeader)[0];
+                break;
+            }
+        }
+        if ($ipList === null || strlen($ipList) === 0) {
+            if ($this->attributeIps !== null) {
+                $request = $request->withAttribute($this->attributeIps, []);
+            }
+            return $handler->handle($request);
+        }
+
+        $ipList = preg_split('/\s*,\s*/', trim($ipList), -1, PREG_SPLIT_NO_EMPTY);
+        do {
+            $ip = array_pop($ipList);
+            if (!$this->isValidHost($ip, ['any'], $ipValidator)) {
+                break;
+            }
+            $ips[] = $ip;
+            if (!$this->isValidHost($ip, $trustedHostData['hosts'], $ipValidator)) {
+                break;
+            }
+        } while (count($ipList) > 0);
+
+        if ($this->attributeIps !== null) {
+            $request = $request->withAttribute($this->attributeIps, $ips);
+        }
+
+        return $handler->handle($request->withAttribute('clientIp', end($ips)));
     }
 }
---
+++ tests/Middleware/TrustedHostsNetworkResolverTest.php
@@ -0,0 +1,87 @@
+<?php
+
+
+namespace Yiisoft\Yii\Web\Tests\Middleware;
+
+
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7\ServerRequest;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Yiisoft\Yii\Web\Middleware\TrustedHostsNetworkResolver;
+use Yiisoft\Yii\Web\Tests\Middleware\Mock\MockRequestHandler;
+
+class TrustedHostsNetworkResolverTest extends TestCase
+{
+    protected function newRequestWithSchemaAndHeaders(
+        string $scheme = 'http',
+        array $headers = [],
+        array $serverParams = []
+    ): ServerRequestInterface {
+        $request = new ServerRequest('GET', '/', $headers, null, '1.1', $serverParams);
+        $uri = $request->getUri()->withScheme($scheme);
+        return $request->withUri($uri);
+    }
+
+    public function notTrustedDataProvider(): array
+    {
+        return [
+            'none' => [
+                [],
+                ['REMOTE_ADDR' => '127.0.0.1'],
+                [],
+            ],
+            'level1' => [
+                ['x-forwarded-for' => '9.9.9.9, 5.5.5.5, 2.2.2.2'],
+                ['REMOTE_ADDR' => '127.0.0.1'],
+                [['hosts' => ['8.8.8.8']]],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider notTrustedDataProvider
+     */
+    public function testNotTrusted(array $headers, array $serverParams, array $trustedHosts)
+    {
+        $request = $this->newRequestWithSchemaAndHeaders('http', $headers, $serverParams);
+        $requestHandler = new MockRequestHandler();
+
+        $middleware = new TrustedHostsNetworkResolver(new Psr17Factory());
+        foreach ($trustedHosts as $data) {
+            $middleware = $middleware->withAddedTrustedHosts($data['hosts'], $data['ipHeaders'] ?? null,
+                $data['protocolHeaders'] ?? null, $data['trustedHeaders'] ?? null);
+        }
+        $response = $middleware->process($request, $requestHandler);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertSame(412, $response->getStatusCode());
+    }
+
+    public function testNotTrustedMiddleware()
+    {
+        $request = $this->newRequestWithSchemaAndHeaders('http', [], [
+            'REMOTE_ADDR' => '127.0.0.1',
+        ]);
+        $requestHandler = new MockRequestHandler();
+
+        $middleware = new TrustedHostsNetworkResolver(new Psr17Factory());
+        $middleware = $middleware->withNotTrustedBranch(new class() implements MiddlewareInterface
+        {
+
+            public function process(
+                ServerRequestInterface $request,
+                RequestHandlerInterface $handler
+            ): ResponseInterface {
+                $response = (new Psr17Factory())->createResponse(200);
+                $response->getBody()->write('Another branch.');
+                return $response;
+            }
+        });
+        $response = $middleware->process($request, $requestHandler);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertSame(200, $response->getStatusCode());
+    }
+}
