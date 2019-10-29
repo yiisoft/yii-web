e1b36b1120291e17465128555d98fcb43bdfc1af
romkatsu <r@tsurkanu.ru>
2019-10-29 19:52:32 +0000

    add not found handler tests

---
+++ tests/NotFoundHandlerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Yiisoft\Yii\Web\Tests;
+
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7\ServerRequest;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use Yiisoft\Router\Method;
+use Yiisoft\Yii\Web\NotFoundHandler;
+
+final class NotFoundHandlerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function returnCode404()
+    {
+        $response = $this->createHandler()->handle($this->createRequest());
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    /**
+     * @test
+     */
+    public function returnValidMessage()
+    {
+        $response = $this->createHandler()->handle($this->createRequest('http://site.com/test/path?param=1'));
+        $this->assertEquals('We were unable to find the page /test/path.', (string)$response->getBody());
+    }
+
+    private function createHandler(): NotFoundHandler
+    {
+        return new NotFoundHandler(new Psr17Factory());
+    }
+
+    private function createRequest(string $uri = '/'): ServerRequestInterface
+    {
+        return new ServerRequest(Method::GET, $uri);
+    }
+}
