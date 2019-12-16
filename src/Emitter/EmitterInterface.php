27cffd80859489afdf19da1df53a130208670e0d
roxblnfk <roxblnfk@ya.ru>
2019-12-16 19:16:25 +0300

    SapiEmitter now throws Exception when HTTP headers have been sent

--- src/Emitter/EmitterInterface.php
+++ src/Emitter/EmitterInterface.php
@@ -3,6 +3,7 @@
 namespace Yiisoft\Yii\Web\Emitter;
 
 use Psr\Http\Message\ResponseInterface;
+use Yiisoft\Yii\Web\Exception\HeadersHaveBeenSentException;
 
 /**
  * Emitter takes PSR-7 ResponseInterface, formats and outputs it
@@ -13,6 +14,7 @@ interface EmitterInterface
      * @param ResponseInterface $response
      * @param bool $withoutBody if body should be omitted
      * @return bool whether the response have been outputted successfully
+     * @throws HeadersHaveBeenSentException
      */
     public function emit(ResponseInterface $response, bool $withoutBody = false): bool;
 }
--- tests/Emitter/SapiEmitterTest.php
+++ tests/Emitter/SapiEmitterTest.php
@@ -7,8 +7,11 @@ use Nyholm\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamInterface;
+use Yiisoft\FriendlyException\FriendlyExceptionInterface;
 use Yiisoft\Yii\Web\Emitter\EmitterInterface;
 use Yiisoft\Yii\Web\Emitter\SapiEmitter;
+use Yiisoft\Yii\Web\Exception\HeadersHaveBeenSentException;
+
 
 /**
  * @runTestsInSeparateProcesses
@@ -134,6 +137,19 @@ class SapiEmitterTest extends TestCase
 
         $this->assertEquals(['Content-Length: ' . strlen($body)], $this->getHeaders());
         $this->expectOutputString($body);
+    }
+
+    /**
+     * @test
+     */
+    public function exceptionWhenHeadersHaveBeenSent(): void
+    {
+        $body = 'Example body';
+        $response = $this->createResponse(200, [], $body);
+        HTTPFunctions::set_headers_sent(true, 'test-file.php', 200);
+
+        $this->expectException(HeadersHaveBeenSentException::class);
+        $this->createEmitter()->emit($response);
     }
 
     /**
