df8e9554c97a1f678c4d14151dc9d9c6075914af
Rustam Mamadaminov <rmamdaminov@gmail.com>
2020-01-11 23:22:52 +0500

    Added tests

--- tests/Middleware/HttpCacheTest.php
+++ tests/Middleware/HttpCacheTest.php
@@ -17,7 +17,19 @@ class HttpCacheTest extends TestCase
     /**
      * @test
      */
-    public function modifiedResult(): void
+    public function notCacheableMethods(): void
+    {
+        $time = \time();
+        $middleware = $this->createMiddlewareWithLastModified($time + 1);
+        $response = $middleware->process($this->createServerRequest(Method::PATCH), $this->createRequestHandler());
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertFalse($response->hasHeader('Last-Modified'));
+    }
+
+    /**
+     * @test
+     */
+    public function modifiedResultWithLastModified(): void
     {
         $time = \time();
         $middleware = $this->createMiddlewareWithLastModified($time + 1);
@@ -31,7 +43,22 @@ class HttpCacheTest extends TestCase
     /**
      * @test
      */
-    public function notModifiedResult(): void
+    public function modifiedResultWithEtag(): void
+    {
+        $etag = 'test-etag';
+        $middleware = $this->createMiddlewareWithETag($etag);
+        $headers = [
+            'If-None-Match' => implode(',', [$etag]),
+        ];
+        $response = $middleware->process($this->createServerRequest(Method::GET, $headers), $this->createRequestHandler());
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals($response->getHeaderLine('Etag'), $this->generateEtag($etag));
+    }
+
+    /**
+     * @test
+     */
+    public function notModifiedResultWithLastModified(): void
     {
         $time = \time();
         $middleware = $this->createMiddlewareWithLastModified($time - 1);
@@ -44,11 +71,35 @@ class HttpCacheTest extends TestCase
         $this->assertEquals(gmdate('D, d M Y H:i:s', $time - 1) . ' GMT', $response->getHeaderLine('Last-Modified'));
     }
 
+    /**
+     * @test
+     */
+    public function notModifiedResultWithEtag(): void
+    {
+        $etag = 'test-etag';
+        $middleware = $this->createMiddlewareWithETag($etag);
+        $headers = [
+            'If-None-Match' => implode(',', [$this->generateEtag($etag)]),
+        ];
+        $response = $middleware->process($this->createServerRequest(Method::GET, $headers), $this->createRequestHandler());
+        $this->assertEquals(304, $response->getStatusCode());
+        $this->assertEmpty((string)$response->getBody());
+    }
+
     private function createMiddlewareWithLastModified(int $lastModified): HttpCache
     {
         $middleware = new HttpCache(new Psr17Factory());
         $middleware->setLastModified(static function (ServerRequestInterface $request, $params) use ($lastModified) {
             return $lastModified;
+        });
+        return $middleware;
+    }
+
+    private function createMiddlewareWithETag(string $etag): HttpCache
+    {
+        $middleware = new HttpCache(new Psr17Factory());
+        $middleware->setEtagSeed(static function (ServerRequestInterface $request, $params) use ($etag) {
+            return $etag;
         });
         return $middleware;
     }
@@ -66,5 +117,11 @@ class HttpCacheTest extends TestCase
     private function createServerRequest(string $method = Method::GET, $headers = []): ServerRequestInterface
     {
         return new ServerRequest($method, '/', $headers);
+    }
+
+    private function generateEtag(string $seed, ?string $weakEtag = null): string
+    {
+        $etag = '"' . rtrim(base64_encode(sha1($seed, true)), '=') . '"';
+        return $weakEtag ? 'W/' . $etag : $etag;
     }
 }
