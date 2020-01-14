ce8041ed356db309fcca92d4ac8eebbcbb8e907f
Rustam Mamadaminov <rmamdaminov@gmail.com>
2020-01-14 16:55:12 +0500

    Implemented HttpCache middleware (#199)
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

---
+++ src/Middleware/HttpCache.php
@@ -0,0 +1,202 @@
+<?php
+namespace Yiisoft\Yii\Web\Middleware;
+
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Yiisoft\Http\Method;
+
+/**
+ * HttpCache implements client-side caching by utilizing the `Last-Modified` and `ETag` HTTP headers.
+ */
+final class HttpCache implements MiddlewareInterface
+{
+    private const DEFAULT_HEADER = 'public, max-age=3600';
+
+    /**
+     * @var callable a PHP callback that returns the UNIX timestamp of the last modification time.
+     * The callback's signature should be:
+     *
+     * ```php
+     * function (ServerRequestInterface $request, $params): int
+     * ```
+     *
+     * where `$request` is the {@see ServerRequestInterface} object that this filter is currently handling;
+     * `$params` takes the value of {@see params}. The callback should return a UNIX timestamp.
+     *
+     * @see http://tools.ietf.org/html/rfc7232#section-2.2
+     */
+    private $lastModified;
+
+    /**
+     * @var callable a PHP callback that generates the ETag seed string.
+     * The callback's signature should be:
+     *
+     * ```php
+     * function (ServerRequestInterface $request, $params): string
+     * ```
+     *
+     * where `$request` is the {@see ServerRequestInterface} object that this middleware is currently handling;
+     * `$params` takes the value of {@see $params}. The callback should return a string serving
+     * as the seed for generating an ETag.
+     */
+    private $etagSeed;
+
+    /**
+     * @var bool whether to generate weak ETags.
+     *
+     * Weak ETags should be used if the content should be considered semantically equivalent, but not byte-equal.
+     *
+     * @see http://tools.ietf.org/html/rfc7232#section-2.3
+     */
+    private bool $weakEtag = false;
+
+    /**
+     * @var mixed additional parameters that should be passed to the {@see $lastModified} and {@see etagSeed} callbacks.
+     */
+    private $params;
+
+    /**
+     * @var string the value of the `Cache-Control` HTTP header. If null, the header will not be sent.
+     * @see http://tools.ietf.org/html/rfc2616#section-14.9
+     */
+    private ?string $cacheControlHeader = self::DEFAULT_HEADER;
+
+    private ResponseFactoryInterface $responseFactory;
+
+    public function __construct(ResponseFactoryInterface $responseFactory)
+    {
+        $this->responseFactory = $responseFactory;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if (
+            ($this->lastModified === null && $this->etagSeed === null) ||
+            !\in_array($request->getMethod(), [Method::GET, Method::HEAD], true)
+        ) {
+            return $handler->handle($request);
+        }
+
+        $lastModified = null;
+        if ($this->lastModified !== null) {
+            $lastModified = call_user_func($this->lastModified, $request, $this->params);
+        }
+
+        $etag = null;
+        if ($this->etagSeed !== null) {
+            $seed = call_user_func($this->etagSeed, $request, $this->params);
+            if ($seed !== null) {
+                $etag = $this->generateEtag($seed);
+            }
+        }
+
+        $cacheIsValid = $this->validateCache($request, $lastModified, $etag);
+        if ($cacheIsValid) {
+            return $this->responseFactory
+                ->createResponse(304)
+                ->withHeader(
+                    'Last-Modified',
+                    gmdate('D, d M Y H:i:s', $lastModified) . ' GMT'
+                );
+        }
+
+        $response = $handler->handle($request);
+        if ($this->cacheControlHeader !== null) {
+            $response = $response->withHeader('Cache-Control', $this->cacheControlHeader);
+        }
+        if ($etag !== null) {
+            $response = $response->withHeader('Etag', $etag);
+        }
+
+        // https://tools.ietf.org/html/rfc7232#section-4.1
+        if ($lastModified !== null && (!$cacheIsValid || ($cacheIsValid && $etag === null))) {
+            $response = $response->withHeader(
+                'Last-Modified',
+                gmdate('D, d M Y H:i:s', $lastModified) . ' GMT'
+            );
+        }
+
+        return $response;
+    }
+
+    /**
+     * Validates if the HTTP cache contains valid content.
+     * If both Last-Modified and ETag are null, returns false.
+     * @param ServerRequestInterface $request
+     * @param int|null $lastModified the calculated Last-Modified value in terms of a UNIX timestamp.
+     * If null, the Last-Modified header will not be validated.
+     * @param string|null $etag the calculated ETag value. If null, the ETag header will not be validated.
+     * @return bool whether the HTTP cache is still valid.
+     */
+    private function validateCache(ServerRequestInterface $request, ?int $lastModified, ?string $etag): bool
+    {
+        if ($request->hasHeader('If-None-Match')) {
+            // HTTP_IF_NONE_MATCH takes precedence over HTTP_IF_MODIFIED_SINCE
+            // http://tools.ietf.org/html/rfc7232#section-3.3
+            return $etag !== null && \in_array($etag, $this->getETags($request), true);
+        }
+
+        if ($request->hasHeader('If-Modified-Since')) {
+            $header = $request->getHeaderLine('If-Modified-Since');
+            return $lastModified !== null && @strtotime($header) >= $lastModified;
+        }
+
+        return false;
+    }
+
+    /**
+     * Generates an ETag from the given seed string.
+     * @param string $seed Seed for the ETag
+     * @return string the generated ETag
+     */
+    private function generateEtag(string $seed): string
+    {
+        $etag = '"' . rtrim(base64_encode(sha1($seed, true)), '=') . '"';
+        return $this->weakEtag ? 'W/' . $etag : $etag;
+    }
+
+    /**
+     * Gets the Etags.
+     *
+     * @param ServerRequestInterface $request
+     * @return array The entity tags
+     */
+    private function getETags(ServerRequestInterface $request): array
+    {
+        if ($request->hasHeader('If-None-Match')) {
+            $header = $request->getHeaderLine('If-None-Match');
+            $header = \str_replace('-gzip', '', $header);
+            return \preg_split('/[\s,]+/', $header, -1, PREG_SPLIT_NO_EMPTY) ?: [];
+        }
+
+        return [];
+    }
+
+    public function setLastModified(callable $lastModified): void
+    {
+        $this->lastModified = $lastModified;
+    }
+
+    public function setEtagSeed(callable $etagSeed): void
+    {
+        $this->etagSeed = $etagSeed;
+    }
+
+    public function setWeakTag(bool $weakTag): void
+    {
+        $this->weakEtag = $weakTag;
+    }
+
+    public function setParams($params): void
+    {
+        $this->params = $params;
+    }
+
+    public function setCacheControlHeader(?string $header): void
+    {
+        $this->cacheControlHeader = $header;
+    }
+}
---
+++ tests/Middleware/HttpCacheTest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Yiisoft\Yii\Web\Tests\Middleware;
+
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7\Response;
+use Nyholm\Psr7\ServerRequest;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Yiisoft\Http\Method;
+use Yiisoft\Yii\Web\Middleware\HttpCache;
+
+class HttpCacheTest extends TestCase
+{
+    public function testNotCacheableMethods(): void
+    {
+        $time = \time();
+        $middleware = $this->createMiddlewareWithLastModified($time + 1);
+        $response = $middleware->process($this->createServerRequest(Method::PATCH), $this->createRequestHandler());
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertFalse($response->hasHeader('Last-Modified'));
+    }
+
+    public function testModifiedResultWithLastModified(): void
+    {
+        $time = \time();
+        $middleware = $this->createMiddlewareWithLastModified($time + 1);
+        $headers = [
+            'If-Modified-Since' => gmdate('D, d M Y H:i:s', $time) . 'GMT',
+        ];
+        $response = $middleware->process($this->createServerRequest(Method::GET, $headers), $this->createRequestHandler());
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testModifiedResultWithEtag(): void
+    {
+        $etag = 'test-etag';
+        $middleware = $this->createMiddlewareWithETag($etag);
+        $headers = [
+            'If-None-Match' => $etag,
+        ];
+        $response = $middleware->process($this->createServerRequest(Method::GET, $headers), $this->createRequestHandler());
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals($response->getHeaderLine('Etag'), $this->generateEtag($etag));
+    }
+
+    public function testNotModifiedResultWithLastModified(): void
+    {
+        $time = \time();
+        $middleware = $this->createMiddlewareWithLastModified($time - 1);
+        $headers = [
+            'If-Modified-Since' => gmdate('D, d M Y H:i:s', $time) . 'GMT',
+        ];
+        $response = $middleware->process($this->createServerRequest(Method::GET, $headers), $this->createRequestHandler());
+        $this->assertEquals(304, $response->getStatusCode());
+        $this->assertEmpty((string)$response->getBody());
+        $this->assertEquals(gmdate('D, d M Y H:i:s', $time - 1) . ' GMT', $response->getHeaderLine('Last-Modified'));
+    }
+
+    public function testNotModifiedResultWithEtag(): void
+    {
+        $etag = 'test-etag';
+        $middleware = $this->createMiddlewareWithETag($etag);
+        $headers = [
+            'If-None-Match' => $this->generateEtag($etag),
+        ];
+        $response = $middleware->process($this->createServerRequest(Method::GET, $headers), $this->createRequestHandler());
+        $this->assertEquals(304, $response->getStatusCode());
+        $this->assertEmpty((string)$response->getBody());
+    }
+
+    private function createMiddlewareWithLastModified(int $lastModified): HttpCache
+    {
+        $middleware = new HttpCache(new Psr17Factory());
+        $middleware->setLastModified(static function (ServerRequestInterface $request, $params) use ($lastModified) {
+            return $lastModified;
+        });
+        return $middleware;
+    }
+
+    private function createMiddlewareWithETag(string $etag): HttpCache
+    {
+        $middleware = new HttpCache(new Psr17Factory());
+        $middleware->setEtagSeed(static function (ServerRequestInterface $request, $params) use ($etag) {
+            return $etag;
+        });
+        return $middleware;
+    }
+
+    private function createRequestHandler(): RequestHandlerInterface
+    {
+        return new class implements RequestHandlerInterface {
+            public function handle(ServerRequestInterface $request): ResponseInterface
+            {
+                return new Response(200);
+            }
+        };
+    }
+
+    private function createServerRequest(string $method = Method::GET, $headers = []): ServerRequestInterface
+    {
+        return new ServerRequest($method, '/', $headers);
+    }
+
+    private function generateEtag(string $seed, ?string $weakEtag = null): string
+    {
+        $etag = '"' . rtrim(base64_encode(sha1($seed, true)), '=') . '"';
+        return $weakEtag ? 'W/' . $etag : $etag;
+    }
+}
