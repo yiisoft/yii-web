f0bc509235319f10e5a273b4883b3a27703ee10e
Dmitriy Derepko <xepozz@list.ru>
2020-04-12 20:42:56 +0300

    Rename set

--- .phpstorm.meta.php/Cookie.php
+++ .phpstorm.meta.php/Cookie.php
@@ -5,11 +5,11 @@ namespace PHPSTORM_META {
     expectedArguments(
         \Yiisoft\Yii\Web\Cookie::sameSite(),
         0,
-        argumentsSet('yiisoft/yii-web/cookie'),
+        argumentsSet('\Yiisoft\Yii\Web\Cookie::SAME_SITE'),
     );
 
     registerArgumentsSet(
-        'yiisoft/yii-web/cookie',
+        '\Yiisoft\Yii\Web\Cookie::SAME_SITE',
         \Yiisoft\Yii\Web\Cookie::SAME_SITE_LAX,
         \Yiisoft\Yii\Web\Cookie::SAME_SITE_STRICT,
         \Yiisoft\Yii\Web\Cookie::SAME_SITE_NONE,
