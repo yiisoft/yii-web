2a7a21e12f569616ef3c09a2ed50a70d719cabfc
Dmitriy Derepko <xepozz@list.ru>
2020-04-13 00:44:21 +0300

    Refactored a little bit (#242)

--- src/Config/EventConfigurator.php
+++ src/Config/EventConfigurator.php
@@ -22,19 +22,22 @@ class EventConfigurator extends AbstractProviderConfigurator
     public function registerListeners(array $listeners): void
     {
         foreach ($listeners as $eventName => $listener) {
-            if (is_string($eventName)) {
-                foreach ($listener as $callable) {
-                    if (!is_callable($callable)) {
-                        throw new \RuntimeException('Listener must be a callable.');
-                    }
-                    if (is_array($callable) && !is_object($callable[0])) {
-                        $callable = [$this->container->get($callable[0]), $callable[1]];
-                    }
-                    $this->listenerProvider
-                        ->attach(fn ($event) => (new Injector($this->container))->invoke($callable, [$event]), $eventName);
-                }
-            } else {
+            if (!is_string($eventName)) {
                 throw new \RuntimeException('Incorrect event listener format. Format with event name must be used.');
+            }
+
+            foreach ($listener as $callable) {
+                if (!is_callable($callable)) {
+                    throw new \RuntimeException('Listener must be a callable.');
+                }
+                if (is_array($callable) && !is_object($callable[0])) {
+                    $callable = [$this->container->get($callable[0]), $callable[1]];
+                }
+                $this->listenerProvider
+                    ->attach(
+                        fn($event) => (new Injector($this->container))->invoke($callable, [$event]),
+                        $eventName
+                    );
             }
         }
     }
