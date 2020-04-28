23259ed6658cd6f0417960c7a68f9f6a45833261
Arman Poghosyan <armpogart@users.noreply.github.com>
2020-04-28 21:50:29 +0400

    Refactor Cookie, add CookieCollection (#244)

---
+++ tests/CookieCollectionTest.php
@@ -0,0 +1,250 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\Web\Tests;
+
+use Nyholm\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Yii\Web\Cookie;
+use Yiisoft\Yii\Web\CookieCollection;
+
+final class CookieCollectionTest extends TestCase
+{
+    private CookieCollection $collection;
+
+    protected function setUp(): void
+    {
+        $this->collection = new CookieCollection([]);
+    }
+
+    public function testConstructorWithInvalidArray(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new CookieCollection([new Cookie('test'), 'string']);
+    }
+
+    public function testIssetAndUnset(): void
+    {
+        $this->assertFalse(isset($this->collection['test']));
+        $this->collection->add(new Cookie('test'));
+        $this->assertTrue(isset($this->collection['test']));
+        unset($this->collection['test']);
+        $this->assertFalse(isset($this->collection['test']));
+        $this->assertCount(0, $this->collection);
+    }
+
+    public function testRemovingNonExistingEntryReturnsNull(): void
+    {
+        $this->assertEquals(null, $this->collection->remove('key_does_not_exist'));
+    }
+
+    public function testExists(): void
+    {
+        $this->collection->add(new Cookie('one', 'oneValue'));
+        $this->collection->add(new Cookie('two', 'twoValue'));
+        $exists = $this->collection->exists(static function (Cookie $cookie, string $name) {
+            return $name === 'one' && $cookie->getValue() === 'oneValue';
+        });
+        $this->assertTrue($exists);
+        $exists = $this->collection->exists(static function (Cookie $cookie, string $name) {
+            return $name === 'two' && $cookie->getValue() === 'wrongValue';
+        });
+        $this->assertFalse($exists);
+    }
+
+    public function testWalk(): void
+    {
+        $this->collection->add(new Cookie('one', 'oneValue'));
+        $this->collection->add(new Cookie('two', 'twoValue'));
+        $this->collection->walk(static function (Cookie &$cookie, string $name) {
+            if ($name === 'two') {
+                $cookie = $cookie->withValue('modifiedTwoValue');
+            }
+        });
+
+        $this->assertEquals('modifiedTwoValue', $this->collection->get('two')->getValue());
+    }
+
+    public function testArrayAccess(): void
+    {
+        $cookieOne = new Cookie('one');
+        $cookieTwo = new Cookie('two');
+
+        $this->collection[] = $cookieOne;
+        $this->collection['two'] = $cookieTwo;
+
+        $this->assertEquals($cookieOne, $this->collection['one']);
+        $this->assertEquals($cookieTwo, $this->collection['two']);
+
+        $this->assertCount(2, $this->collection);
+    }
+
+    public function testContains(): void
+    {
+        $cookie = new Cookie('test');
+        $this->collection->add($cookie);
+        $this->assertTrue($this->collection->contains($cookie));
+    }
+
+    public function testGet(): void
+    {
+        $cookie = new Cookie('test');
+        $this->collection->add($cookie);
+        $this->assertEquals($cookie, $this->collection->get('test'));
+    }
+
+    public function testGetValue(): void
+    {
+        $this->collection->add(new Cookie('test', 'testVal'));
+        $this->assertEquals('testVal', $this->collection->getValue('test'));
+    }
+
+    public function testGetKeys(): void
+    {
+        $this->collection->add(new Cookie('one'));
+        $this->collection->add(new Cookie('two'));
+        $this->assertEquals(['one', 'two'], $this->collection->getKeys());
+    }
+
+    public function testGetValues(): void
+    {
+        $cookieOne = new Cookie('one');
+        $cookieTwo = new Cookie('two');
+        $collection = new CookieCollection([$cookieOne, $cookieTwo]);
+        $this->assertEquals([$cookieOne, $cookieTwo], $collection->getValues());
+    }
+
+    public function testCount(): void
+    {
+        $this->collection[] = new Cookie('one');
+        $this->collection[] = new Cookie('two');
+        $this->assertEquals(2, $this->collection->count());
+        $this->assertCount(2, $this->collection);
+    }
+
+    public function testClear(): void
+    {
+        $this->collection[] = new Cookie('one');
+        $this->collection[] = new Cookie('two');
+        $this->collection->clear();
+        $this->assertEmpty($this->collection);
+    }
+
+    public function testIsEmpty(): void
+    {
+        $this->assertTrue($this->collection->isEmpty());
+    }
+
+    public function testRemove(): void
+    {
+        $cookieOne = new Cookie('one');
+        $cookieTwo = new Cookie('two');
+        $collection = new CookieCollection([$cookieOne, $cookieTwo]);
+
+        $cookie = $collection->remove('one');
+        $this->assertNotNull($cookie);
+        $this->assertEquals($cookieOne, $cookie);
+        $this->assertFalse($collection->has($cookie->getName()));
+        $this->assertNull($collection->remove('one'));
+    }
+
+    public function testToArray(): void
+    {
+        $cookieOne = new Cookie('one');
+        $cookieTwo = new Cookie('two');
+        $collection = new CookieCollection([$cookieOne, $cookieTwo]);
+
+        $expected = ['one' => $cookieOne, 'two' => $cookieTwo];
+        $this->assertEquals($expected, $collection->toArray());
+    }
+
+    public function testIterator(): void
+    {
+        $cookieOne = new Cookie('one');
+        $cookieTwo = new Cookie('two');
+        $this->collection->add($cookieOne);
+        $this->collection->add($cookieTwo);
+
+        $this->assertIsIterable($this->collection);
+    }
+
+    public function testExpire(): void
+    {
+        $this->collection->add(new Cookie('test'));
+        $this->collection->expire('test');
+
+        $this->assertTrue($this->collection->get('test')->isExpired());
+    }
+
+    public function testFromArray(): void
+    {
+        $cookieArray = ['one' => 'oneValue', 'two' => 'twoValue'];
+        $collection = CookieCollection::fromArray($cookieArray);
+
+        $this->assertCount(2, $collection);
+        $this->assertInstanceOf(Cookie::class, $collection['one']);
+    }
+
+    public function testFromArrayWithInvalidArray(): void
+    {
+        $cookieArray = ['one', 'two'];
+
+        $this->expectException(\InvalidArgumentException::class);
+        CookieCollection::fromArray($cookieArray);
+    }
+
+    public function testFromArrayWithMalformedArray(): void
+    {
+        $cookieArray = ['one' => 'oneValue', 'two'];
+
+        $this->expectException(\InvalidArgumentException::class);
+        CookieCollection::fromArray($cookieArray);
+    }
+
+    public function testFromArrayWithEmptyArray(): void
+    {
+        $collection = CookieCollection::fromArray([]);
+
+        $this->assertCount(0, $collection);
+    }
+
+    public function testFromArrayWithInvalidArgument(): void
+    {
+        $cookieArray = ['one', 'two'];
+        $this->expectException(\InvalidArgumentException::class);
+        CookieCollection::fromArray($cookieArray);
+    }
+
+    public function testAddToResponse(): void
+    {
+        $response = (new Response())->withHeader('Set-Cookie', 'oldCookie=oldValue;Secure');
+        $this->collection->add(new Cookie('one', 'oneValue'));
+        $this->collection->add(new Cookie('two', 'twoValue', new \DateTimeImmutable()));
+
+        $response = $this->collection->addToResponse($response);
+        $this->assertCount(3, $response->getHeader('Set-Cookie'));
+        $this->assertEquals('oldCookie=oldValue;Secure', $response->getHeader('Set-Cookie')[0]);
+    }
+
+    public function testSetToResponse(): void
+    {
+        $response = (new Response())->withHeader('Set-Cookie', 'oldCookie=oldValue;Secure');
+        $this->collection->add(new Cookie('one', 'oneValue'));
+        $this->collection->add(new Cookie('two', 'twoValue', new \DateTimeImmutable()));
+
+        $response = $this->collection->setToResponse($response);
+        $this->assertCount(2, $response->getHeader('Set-Cookie'));
+        $this->assertEquals('one=oneValue; Path=/; Secure; HttpOnly; SameSite=Lax', $response->getHeader('Set-Cookie')[0]);
+    }
+
+    public function testFromResponse(): void
+    {
+        $response = new Response();
+        $response = $response->withAddedHeader('Set-Cookie', 'one=oneValue; Path=/; Secure; HttpOnly; SameSite=Lax');
+        $response = $response->withAddedHeader('Set-Cookie', 'two=twoValue; Path=/; Secure; HttpOnly; SameSite=Lax');
+
+        $collection = CookieCollection::fromResponse($response);
+        $this->assertCount(2, $collection);
+    }
+}
--- tests/CookieTest.php
+++ tests/CookieTest.php
@@ -6,7 +6,7 @@ use Nyholm\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 use Yiisoft\Yii\Web\Cookie;
 
-class CookieTest extends TestCase
+final class CookieTest extends TestCase
 {
     private function getCookieHeader(Cookie $cookie): string
     {
@@ -28,22 +28,65 @@ class CookieTest extends TestCase
         $this->assertSame('test=42; Path=/; Secure; HttpOnly; SameSite=Lax', $this->getCookieHeader($cookie));
     }
 
-    public function testDomain(): void
+    public function testWithValue(): void
     {
-        $cookie = (new Cookie('test', 42))->domain('yiiframework.com');
-
-        $this->assertSame('test=42; Path=/; Domain=yiiframework.com; Secure; HttpOnly; SameSite=Lax', $this->getCookieHeader($cookie));
+        $cookie = (new Cookie('test'))->withValue(42);
+        $this->assertSame('test=42; Path=/; Secure; HttpOnly; SameSite=Lax', $this->getCookieHeader($cookie));
     }
 
-    public function testExpireAt(): void
+    public function testInvalidValue(): void
     {
-        $expireDateTime = new \DateTime();
+        $this->expectException(\InvalidArgumentException::class);
+        (new Cookie('test'))->withValue(';');
+    }
+
+    public function testWithExpires(): void
+    {
+        $expireDateTime = new \DateTime('+1 year');
         $expireDateTime->setTimezone(new \DateTimeZone('GMT'));
-        $formattedDateTime = $expireDateTime->format('D, d-M-Y H:i:s T');
+        $formattedDateTime = $expireDateTime->format(\DateTimeInterface::RFC7231);
+        $maxAge = $expireDateTime->getTimestamp() - time();
 
-        $cookie = (new Cookie('test', 42))->expireAt($expireDateTime);
+        $cookie = (new Cookie('test', 42))->withExpires($expireDateTime);
 
-        $this->assertSame("test=42; Expires=$formattedDateTime; Path=/; Secure; HttpOnly; SameSite=Lax", $this->getCookieHeader($cookie));
+        $this->assertSame("test=42; Expires=$formattedDateTime; Max-Age=$maxAge; Path=/; Secure; HttpOnly; SameSite=Lax", $this->getCookieHeader($cookie));
+    }
+
+    public function testIsExpired(): void
+    {
+        $cookie = (new Cookie('test', 42))->withExpires((new \DateTimeImmutable('-5 years')));
+        $this->assertTrue($cookie->isExpired());
+    }
+
+    public function testWithMaxAge(): void
+    {
+        $formattedExpire = (new \DateTimeImmutable())->setTimestamp(time() + 3600)->format(\DateTimeInterface::RFC7231);
+        $cookie = (new Cookie('test', 42))->withMaxAge(new \DateInterval('PT3600S'));
+
+        $this->assertSame("test=42; Expires=$formattedExpire; Max-Age=3600; Path=/; Secure; HttpOnly; SameSite=Lax", $this->getCookieHeader($cookie));
+    }
+
+    public function testExpire(): void
+    {
+        $cookie = (new Cookie('test', 42))->expire();
+        $this->assertTrue($cookie->isExpired());
+    }
+
+    public function testNegativeInterval(): void
+    {
+        $formattedExpire = (new \DateTimeImmutable())->setTimestamp(time() - 3600)->format(\DateTimeInterface::RFC7231);
+        $negativeInterval = new \DateInterval('PT3600S');
+        $negativeInterval->invert = 1;
+        $cookie = (new Cookie('test', 42))->withMaxAge($negativeInterval);
+
+        $this->assertSame("test=42; Expires=$formattedExpire; Max-Age=-3600; Path=/; Secure; HttpOnly; SameSite=Lax", $this->getCookieHeader($cookie));
+    }
+
+    public function testWithDomain(): void
+    {
+        $cookie = (new Cookie('test', 42))->withDomain('yiiframework.com');
+
+        $this->assertSame('test=42; Domain=yiiframework.com; Path=/; Secure; HttpOnly; SameSite=Lax', $this->getCookieHeader($cookie));
     }
 
     public function testExpireWhenBrowserIsClosed(): void
@@ -53,23 +96,29 @@ class CookieTest extends TestCase
         $this->assertSame('test=42; Path=/; Secure; HttpOnly; SameSite=Lax', $this->getCookieHeader($cookie));
     }
 
-    public function testPath(): void
+    public function testWithPath(): void
     {
-        $cookie = (new Cookie('test', 42))->path('/test');
+        $cookie = (new Cookie('test', 42))->withPath('/test');
 
         $this->assertSame('test=42; Path=/test; Secure; HttpOnly; SameSite=Lax', $this->getCookieHeader($cookie));
     }
 
-    public function testSecure(): void
+    public function testInvalidPath(): void
     {
-        $cookie = (new Cookie('test', 42))->secure(false);
+        $this->expectException(\InvalidArgumentException::class);
+        (new Cookie('test', 42))->withPath(';invalid');
+    }
+
+    public function testWithSecure(): void
+    {
+        $cookie = (new Cookie('test', 42))->withSecure(false);
 
         $this->assertSame('test=42; Path=/; HttpOnly; SameSite=Lax', $this->getCookieHeader($cookie));
     }
 
     public function testHttpOnly(): void
     {
-        $cookie = (new Cookie('test', 42))->httpOnly(false);
+        $cookie = (new Cookie('test', 42))->withHttpOnly(false);
 
         $this->assertSame('test=42; Path=/; Secure; SameSite=Lax', $this->getCookieHeader($cookie));
     }
@@ -78,13 +127,76 @@ class CookieTest extends TestCase
     {
         $this->expectException(\InvalidArgumentException::class);
 
-        (new Cookie('test', 42))->sameSite('invalid');
+        (new Cookie('test', 42))->withSameSite('invalid');
     }
 
-    public function testSameSite(): void
+    public function testSameSiteNone(): void
     {
-        $cookie = (new Cookie('test', 42))->sameSite('');
+        $cookie = (new Cookie('test', 42))->withSameSite(Cookie::SAME_SITE_NONE);
 
-        $this->assertSame('test=42; Path=/; Secure; HttpOnly', $this->getCookieHeader($cookie));
+        $this->assertSame('test=42; Path=/; Secure; HttpOnly; SameSite=None', $this->getCookieHeader($cookie));
+    }
+
+    public function testFromCookieString(): void
+    {
+        $expireDate = new \DateTimeImmutable('+60 minutes');
+        $setCookieString = 'sessionId=e8bb43229de9; Domain=foo.example.com; ';
+        $setCookieString .= 'Expires=' . $expireDate->format(\DateTimeInterface::RFC7231) . '; ';
+        $setCookieString .= 'Max-Age=3600; Path=/; Secure; SameSite=Strict; ExtraKey';
+
+        $cookie = new Cookie(
+            'sessionId',
+            'e8bb43229de9',
+            $expireDate,
+            'foo.example.com',
+            '/',
+            true,
+            false,
+            Cookie::SAME_SITE_STRICT
+        );
+        $cookie2 = Cookie::fromCookieString($setCookieString);
+
+        $this->assertSame((string)$cookie, (string)$cookie2);
+    }
+
+    public function testFromCookieStringWithInvalidString(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        Cookie::fromCookieString('');
+    }
+
+    public function testGetters(): void
+    {
+        $cookie = new Cookie('test', '', null, null, null, null, null, null);
+        $this->assertEquals('test', $cookie->getName());
+        $this->assertEquals('', $cookie->getValue());
+        $this->assertNull($cookie->getExpires());
+        $this->assertNull($cookie->getDomain());
+        $this->assertNull($cookie->getPath());
+        $this->assertFalse($cookie->isSecure());
+        $this->assertFalse($cookie->isHttpOnly());
+        $this->assertNull($cookie->getSameSite());
+
+        $cookie = $cookie->withValue('testValue');
+        $this->assertEquals('testValue', $cookie->getValue());
+
+        $expiry = new \DateTimeImmutable();
+        $cookie = $cookie->withExpires($expiry);
+        $this->assertEquals($expiry->getTimestamp(), $cookie->getExpires()->getTimestamp());
+
+        $cookie = $cookie->withDomain('yiiframework.com');
+        $this->assertEquals('yiiframework.com', $cookie->getDomain());
+
+        $cookie = $cookie->withPath('/path');
+        $this->assertEquals('/path', $cookie->getPath());
+
+        $cookie = $cookie->withSecure(true);
+        $this->assertTrue($cookie->isSecure());
+
+        $cookie = $cookie->withHttpOnly(true);
+        $this->assertTrue($cookie->isHttpOnly());
+
+        $cookie = $cookie->withSameSite(Cookie::SAME_SITE_LAX);
+        $this->assertEquals(Cookie::SAME_SITE_LAX, $cookie->getSameSite());
     }
 }
