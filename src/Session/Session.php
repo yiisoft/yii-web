731cc8b24c6e14721140c0eb6bc59ccc006a82c0
Arman Poghosyan <armpogart@users.noreply.github.com>
2020-05-01 03:22:21 +0400

    Fix #248, Use 5 bits per character for sid, to have valid cookie value

--- src/Session/Session.php
+++ src/Session/Session.php
@@ -13,7 +13,7 @@ class Session implements SessionInterface
         'use_only_cookies' => 1,
         'cookie_httponly' => 1,
         'use_strict_mode' => 1,
-        'sid_bits_per_character' => 6,
+        'sid_bits_per_character' => 5,
         'sid_length' => 48,
         'cache_limiter' => 'nocache',
         'cookie_samesite' => 'Lax',
