003cf4493847f356734051f6aeb63f5adb7c07fc
hubertstrychalski <h.strychalski@timecamp.com>
2020-05-21 19:58:43 +0200

    add tests for XmlDataResponseFormatter

--- tests/Data/Formatter/XmlDataResponseFormatterTest.php
+++ tests/Data/Formatter/XmlDataResponseFormatterTest.php
@@ -39,4 +39,57 @@ class XmlDataResponseFormatterTest extends TestCase
         );
         $this->assertSame(['application/xml; ISO-8859-1'], $result->getHeader('Content-Type'));
     }
+
+    public function testFormatterVersion(): void
+    {
+        $factory = new Psr17Factory();
+        $dataResponse = new DataResponse('test', 200, '', $factory);
+        $formatter = new XmlDataResponseFormatter();
+        $formatter = $formatter->withVersion('1.1');
+        $result = $formatter->format($dataResponse);
+        $result->getBody()->rewind();
+
+        $this->assertSame(
+            "<?xml version=\"1.1\" encoding=\"UTF-8\"?>\n<response>test</response>\n",
+            $result->getBody()->getContents()
+        );
+        $this->assertSame(['application/xml; UTF-8'], $result->getHeader('Content-Type'));
+    }
+
+    public function testFormatterRootTag(): void
+    {
+        $factory = new Psr17Factory();
+        $dataResponse = new DataResponse('test', 200, '', $factory);
+        $formatter = new XmlDataResponseFormatter();
+        $formatter = $formatter->withRootTag('exampleRootTag');
+        $result = $formatter->format($dataResponse);
+        $result->getBody()->rewind();
+
+        $this->assertSame(
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<exampleRootTag>test</exampleRootTag>\n",
+            $result->getBody()->getContents()
+        );
+        $this->assertSame(['application/xml; UTF-8'], $result->getHeader('Content-Type'));
+    }
+
+    public function testFormatterItemTagWhenNameIsEmptyOrInvalid(): void
+    {
+        $factory = new Psr17Factory();
+        $data = [
+            'test',
+            'validName' => 'test',
+            '1_invalidName' => 'test'
+        ];
+        $dataResponse = new DataResponse($data, 200, '', $factory);
+        $formatter = new XmlDataResponseFormatter();
+        $formatter = $formatter->withItemTag('customItemTag');
+        $result = $formatter->format($dataResponse);
+        $result->getBody()->rewind();
+
+        $this->assertSame(
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<response><customItemTag>test</customItemTag><validName>test</validName><customItemTag>test</customItemTag></response>\n",
+            $result->getBody()->getContents()
+        );
+        $this->assertSame(['application/xml; UTF-8'], $result->getHeader('Content-Type'));
+    }
 }
