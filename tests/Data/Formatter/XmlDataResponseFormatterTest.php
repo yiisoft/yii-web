439edc686ad8e4eb58926d08932d6385a6d92e78
Hubert Strychalski <h.strychalski@timecamp.com>
2020-05-25 01:29:56 +0200

    Add unit tests for ErrorHandler and XmlDataResponseFormatter (#255)

--- tests/Data/Formatter/XmlDataResponseFormatterTest.php
+++ tests/Data/Formatter/XmlDataResponseFormatterTest.php
@@ -13,8 +13,8 @@ class XmlDataResponseFormatterTest extends TestCase
     {
         $factory = new Psr17Factory();
         $dataResponse = new DataResponse('test', 200, '', $factory);
-        $formatter = new XmlDataResponseFormatter();
-        $result = $formatter->format($dataResponse);
+        $result = (new XmlDataResponseFormatter())
+            ->format($dataResponse);
         $result->getBody()->rewind();
 
         $this->assertSame(
@@ -28,9 +28,9 @@ class XmlDataResponseFormatterTest extends TestCase
     {
         $factory = new Psr17Factory();
         $dataResponse = new DataResponse('test', 200, '', $factory);
-        $formatter = new XmlDataResponseFormatter();
-        $formatter = $formatter->withEncoding('ISO-8859-1');
-        $result = $formatter->format($dataResponse);
+        $result = (new XmlDataResponseFormatter())
+            ->withEncoding('ISO-8859-1')
+            ->format($dataResponse);
         $result->getBody()->rewind();
 
         $this->assertSame(
@@ -38,5 +38,112 @@ class XmlDataResponseFormatterTest extends TestCase
             $result->getBody()->getContents()
         );
         $this->assertSame(['application/xml; ISO-8859-1'], $result->getHeader('Content-Type'));
+    }
+
+    public function testFormatterVersion(): void
+    {
+        $factory = new Psr17Factory();
+        $dataResponse = new DataResponse('test', 200, '', $factory);
+        $result = (new XmlDataResponseFormatter())
+            ->withVersion('1.1')
+            ->format($dataResponse);
+        $result->getBody()->rewind();
+
+        $this->assertSame(
+            "<?xml version=\"1.1\" encoding=\"UTF-8\"?>\n<response>test</response>\n",
+            $result->getBody()->getContents()
+        );
+        $this->assertSame(['application/xml; UTF-8'], $result->getHeader('Content-Type'));
+    }
+
+    public function testFormatterRootTag(): void
+    {
+        $factory = new Psr17Factory();
+        $dataResponse = new DataResponse('test', 200, '', $factory);
+        $result = (new XmlDataResponseFormatter())
+            ->withRootTag('exampleRootTag')
+            ->format($dataResponse);
+        $result->getBody()->rewind();
+
+        $this->assertSame(
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<exampleRootTag>test</exampleRootTag>\n",
+            $result->getBody()->getContents()
+        );
+        $this->assertSame(['application/xml; UTF-8'], $result->getHeader('Content-Type'));
+    }
+
+    public function testFormatterItemTagWhenNameIsEmptyOrInvalid(): void
+    {
+        $factory = new Psr17Factory();
+        $data = [
+            'test',
+            'validName' => 'test',
+            '1_invalidName' => 'test'
+        ];
+        $dataResponse = new DataResponse($data, 200, '', $factory);
+        $result = (new XmlDataResponseFormatter())
+            ->withItemTag('customItemTag')
+            ->format($dataResponse);
+        $result->getBody()->rewind();
+
+        $this->assertSame(
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<response><customItemTag>test</customItemTag><validName>test</validName><customItemTag>test</customItemTag></response>\n",
+            $result->getBody()->getContents()
+        );
+        $this->assertSame(['application/xml; UTF-8'], $result->getHeader('Content-Type'));
+    }
+
+    public function testFormatterWithObjectTags(): void
+    {
+        $data = new \stdClass();
+        $data->attribute = 'test';
+
+        $factory = new Psr17Factory();
+        $dataResponse = new DataResponse($data, 200, '', $factory);
+        $result = (new XmlDataResponseFormatter())
+            ->withUseObjectTags(true)
+            ->format($dataResponse);
+        $result->getBody()->rewind();
+
+        $this->assertSame(
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<response><stdClass><attribute>test</attribute></stdClass></response>\n",
+            $result->getBody()->getContents()
+        );
+        $this->assertSame(['application/xml; UTF-8'], $result->getHeader('Content-Type'));
+    }
+
+    public function testFormatterWithoutObjectTags(): void
+    {
+        $data = new \stdClass();
+        $data->attribute = 'test';
+
+        $factory = new Psr17Factory();
+        $dataResponse = new DataResponse($data, 200, '', $factory);
+        $result = (new XmlDataResponseFormatter())
+            ->withUseObjectTags(false)
+            ->format($dataResponse);
+        $result->getBody()->rewind();
+
+        $this->assertSame(
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<response><attribute>test</attribute></response>\n",
+            $result->getBody()->getContents()
+        );
+        $this->assertSame(['application/xml; UTF-8'], $result->getHeader('Content-Type'));
+    }
+
+    public function testFormatterWithContentType(): void
+    {
+        $factory = new Psr17Factory();
+        $dataResponse = new DataResponse('test', 200, '', $factory);
+        $result = (new XmlDataResponseFormatter())
+            ->withContentType('text/xml')
+            ->format($dataResponse);
+        $result->getBody()->rewind();
+
+        $this->assertSame(
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<response>test</response>\n",
+            $result->getBody()->getContents()
+        );
+        $this->assertSame(['text/xml; UTF-8'], $result->getHeader('Content-Type'));
     }
 }
