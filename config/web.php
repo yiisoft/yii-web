9eb46185d5f758770f7753d50dbbab41adb8babc
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2020-06-23 17:04:55 -0400

    Adjust config (#265)

--- config/web.php
+++ config/web.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Yiisoft\Aliases\Aliases;
 use Yiisoft\Yii\Web\ErrorHandler\HtmlRenderer;
 use Yiisoft\Yii\Web\ErrorHandler\ThrowableRendererInterface;
@@ -15,10 +17,11 @@ return [
     ],
 
     ThrowableRendererInterface::class => HtmlRenderer::class,
+
     HtmlRenderer::class => [
         '__class' => HtmlRenderer::class,
         '__construct()' => [
-            $params['htmlRenderer']['templates'] ?? [],
+            $params['yiisoft/yii-web']['htmlRenderer']['templates'],
         ],
     ]
 ];
