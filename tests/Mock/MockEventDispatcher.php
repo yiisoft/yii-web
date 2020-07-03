be13a8bfa9ee1c972e93df90052e00b90beb1cd3
romkatsu <r@tsurkanu.ru>
2020-07-03 20:38:55 +0300

    add mocks

---
+++ tests/Mock/MockEventDispatcher.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\Web\Tests\Mock;
+
+use Psr\EventDispatcher\EventDispatcherInterface;
+
+class MockEventDispatcher implements EventDispatcherInterface
+{
+    private array $events = [];
+
+    public function dispatch(object $event): void
+    {
+        $this->events[] = $event;
+    }
+
+    public function getClassesEvents(): array
+    {
+        return array_map(
+            fn($event) => get_class($event),
+            $this->events
+        );
+    }
+
+    public function getEvents(): array
+    {
+        return $this->events;
+    }
+
+    public function getFirstEvent(): ?object
+    {
+        return array_shift($this->events);
+    }
+}
