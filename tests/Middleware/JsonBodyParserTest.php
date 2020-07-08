d71dbea6e83b139e489f1a817b7e9293239363c5
thenotsoft <thenotsoft@gmail.com>
2020-07-08 12:01:06 +0300

    update tests

--- tests/Middleware/JsonBodyParserTest.php
+++ tests/Middleware/JsonBodyParserTest.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\Web\Tests\Middleware;
 
+use Nyholm\Psr7\Response;
+use Nyholm\Psr7\ServerRequest;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Yiisoft\Http\Header;
 use Yiisoft\Yii\Web\Middleware\JsonBodyParser;
 
 final class JsonBodyParserTest extends TestCase
@@ -15,12 +19,19 @@ final class JsonBodyParserTest extends TestCase
     public function testProcess()
     {
         $parser = (new JsonBodyParser());
+
+        $handler = $this->createHandler();
         $parser->process(
-            $this->createMockRequest('{"test":"value"}', ['test' => 'value']),
-            $this->createMock(RequestHandlerInterface::class)
+            $this->createMockRequest('{"test":"value"}'),
+            $handler
         );
 
-        $parser->process($this->createMockRequest('true', null), $this->createMock(RequestHandlerInterface::class));
+        $this->assertSame(['test' => 'value'], $handler->getRequestParsedBody());
+
+        $handler = $this->createHandler();
+        $parser->process($this->createMockRequest('true'), $handler);
+
+        $this->assertNull($handler->getRequestParsedBody());
     }
 
     public function testWithoutAssoc()
@@ -29,10 +40,14 @@ final class JsonBodyParserTest extends TestCase
         $object->test = 'value';
 
         $parser = (new JsonBodyParser(false));
+
+        $handler = $this->createHandler();
         $parser->process(
-            $this->createMockRequest(json_encode($object), $object),
-            $this->createMock(RequestHandlerInterface::class)
+            $this->createMockRequest(json_encode($object)),
+            $handler
         );
+
+        $this->assertEquals($object, $handler->getRequestParsedBody());
     }
 
     public function testThrownException()
@@ -42,34 +57,38 @@ final class JsonBodyParserTest extends TestCase
         $parser = new JsonBodyParser();
         $parser->process(
             $this->createMockRequest('{"test": invalid json}'),
-            $this->createMock(RequestHandlerInterface::class)
+            $this->createHandler()
         );
     }
 
     public function testWithoutThrownException()
     {
         $parser = (new JsonBodyParser(true, 512, JSON_INVALID_UTF8_IGNORE));
+
+        $handler = $this->createHandler();
         $parser->process(
-            $this->createMockRequest('{"test": invalid json}', null),
-            $this->createMock(RequestHandlerInterface::class)
+            $this->createMockRequest('{"test": invalid json}'),
+            $handler
         );
+
+        $this->assertNull($handler->getRequestParsedBody());
     }
 
     public function testIgnoreInvalidUTF8()
     {
         $parser = (new JsonBodyParser());
+
+        $handler = $this->createHandler();
         $parser->process(
-            $this->createMockRequest(
-                '{"test":"value","invalid":"' . chr(193) . '"}',
-                ['test' => 'value', 'invalid' => '']
-            ),
-            $this->createMock(RequestHandlerInterface::class)
+            $this->createMockRequest('{"test":"value","invalid":"' . chr(193) . '"}'),
+            $handler
         );
+
+        $this->assertSame(['test' => 'value', 'invalid' => ''], $handler->getRequestParsedBody());
     }
 
-    private function createMockRequest(string $rawBody, $expect = null): ServerRequestInterface
+    private function createMockRequest(string $rawBody): ServerRequestInterface
     {
-        $request = $this->createMock(ServerRequestInterface::class);
         $body = $this->createMock(StreamInterface::class);
 
         $body
@@ -77,27 +96,27 @@ final class JsonBodyParserTest extends TestCase
             ->method('getContents')
             ->willReturn($rawBody);
 
-        $request
-            ->method('getHeaderLine')
-            ->willReturn('application/json');
+        return new ServerRequest('POST', '/', [Header::CONTENT_TYPE => 'application/json'], $body);
+    }
 
-        $request
-            ->expects($this->once())
-            ->method('getBody')
-            ->willReturn($body);
+    private function createHandler(): RequestHandlerInterface
+    {
+        return new class() implements RequestHandlerInterface {
+            private $requestParsedBody;
 
-        $args = func_get_args();
+            public function handle(ServerRequestInterface $request): ResponseInterface
+            {
+                $this->requestParsedBody = $request->getParsedBody();
+                return new Response();
+            }
 
-        if (count($args) === 2) {
-            $request
-                ->expects($this->once())
-                ->method('withParsedBody')
-                ->with(
-                    $this->equalTo($expect)
-                )
-                ->willReturnSelf();
-        }
-
-        return $request;
+            /**
+             * @return array|object|null
+             */
+            public function getRequestParsedBody()
+            {
+                return $this->requestParsedBody;
+            }
+        };
     }
 }
