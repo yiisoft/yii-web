1ebb6c1f93104163a594645e1a76e0c9a5d1f15f
romkatsu <r@tsurkanu.ru>
2020-08-06 15:08:15 +0300

    add tests

--- tests/Middleware/ErrorCatcherTest.php
+++ tests/Middleware/ErrorCatcherTest.php
@@ -8,6 +8,7 @@ use Nyholm\Psr7\Factory\Psr17Factory;
 use Nyholm\Psr7\ServerRequest;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use Yiisoft\Di\Container;
 use Yiisoft\Log\Logger;
 use Yiisoft\Yii\Web\ErrorHandler\ErrorHandler;
@@ -26,12 +27,35 @@ class ErrorCatcherTest extends TestCase
         $container = $this->getContainerWithThrowableRenderer($containerId, $expectedRendererOutput);
         $mimeType = 'test/test';
         $catcher = $this->getErrorCatcher($container)->withRenderer($mimeType, $containerId);
-        $requestHandler = (new MockRequestHandler())->setHandleExcaption(new \RuntimeException());
+        $response = $catcher->process(
+            new ServerRequest('GET', '/', ['Accept' => [$mimeType]]),
+            $this->getRequestHandler()
+        );
+        $response->getBody()->rewind();
+        $content = $response->getBody()->getContents();
+        $this->assertNotSame(self::DEFAULT_RENDERER_RESPONSE, $content);
+        $this->assertSame($expectedRendererOutput, $content);
+    }
+
+    public function testAddedOnlyOneRenderer(): void
+    {
+        $expectedRendererOutput = 'expectedRendererOutput';
+        $containerId = 'testRenderer';
+        $container = $this->getContainerWithThrowableRenderer($containerId, $expectedRendererOutput);
+        $mimeType = 'test/test';
+        $catcher = $this->getErrorCatcher($container)->withOnlyRenderer($mimeType, $containerId);
+        $requestHandler = $this->getRequestHandler();
+
         $response = $catcher->process(new ServerRequest('GET', '/', ['Accept' => [$mimeType]]), $requestHandler);
         $response->getBody()->rewind();
         $content = $response->getBody()->getContents();
         $this->assertNotSame(self::DEFAULT_RENDERER_RESPONSE, $content);
         $this->assertSame($expectedRendererOutput, $content);
+
+        $response = $catcher->process(new ServerRequest('GET', '/', ['Accept' => ['text/xml']]), $requestHandler);
+        $response->getBody()->rewind();
+        $content = $response->getBody()->getContents();
+        $this->assertSame(self::DEFAULT_RENDERER_RESPONSE, $content);
     }
 
     public function testThrownExceptionWithNotExistsRenderer()
@@ -56,9 +80,13 @@ class ErrorCatcherTest extends TestCase
     public function testWithoutRenderers(): void
     {
         $container = new Container();
-        $catcher = $this->getErrorCatcher($container)->withoutRenderers();
-        $requestHandler = (new MockRequestHandler())->setHandleExcaption(new \RuntimeException());
-        $response = $catcher->process(new ServerRequest('GET', '/', ['Accept' => ['test/html']]), $requestHandler);
+        $response = $this->getErrorCatcher($container)
+            ->withoutRenderers()
+            ->process(
+                new ServerRequest('GET', '/', ['Accept' => ['test/html']]),
+                $this->getRequestHandler()
+            );
+
         $response->getBody()->rewind();
         $content = $response->getBody()->getContents();
         $this->assertSame(self::DEFAULT_RENDERER_RESPONSE, $content);
@@ -67,9 +95,13 @@ class ErrorCatcherTest extends TestCase
     public function testWithoutRenderer(): void
     {
         $container = new Container();
-        $catcher = $this->getErrorCatcher($container)->withoutRenderers('*/*');
-        $requestHandler = (new MockRequestHandler())->setHandleExcaption(new \RuntimeException());
-        $response = $catcher->process(new ServerRequest('GET', '/', ['Accept' => ['test/html']]), $requestHandler);
+        $response = $this->getErrorCatcher($container)
+            ->withoutRenderers('*/*')
+            ->process(
+                new ServerRequest('GET', '/', ['Accept' => ['test/html']]),
+                $this->getRequestHandler()
+            );
+
         $response->getBody()->rewind();
         $content = $response->getBody()->getContents();
         $this->assertSame(self::DEFAULT_RENDERER_RESPONSE, $content);
@@ -82,10 +114,9 @@ class ErrorCatcherTest extends TestCase
         $container = $this->getContainerWithThrowableRenderer($containerId, $expectedRendererOutput);
         $mimeType = 'text/html;version=2';
         $catcher = $this->getErrorCatcher($container)->withRenderer($mimeType, $containerId);
-        $requestHandler = (new MockRequestHandler())->setHandleExcaption(new \RuntimeException());
         $response = $catcher->process(
             new ServerRequest('GET', '/', ['Accept' => ['text/html', $mimeType]]),
-            $requestHandler
+            $this->getRequestHandler()
         );
         $response->getBody()->rewind();
         $content = $response->getBody()->getContents();
@@ -99,10 +130,9 @@ class ErrorCatcherTest extends TestCase
         $container = $this->getContainerWithThrowableRenderer($containerId, $expectedRendererOutput);
         $catcher = $this->getErrorCatcher($container)
             ->withRenderer('*/*', $containerId);
-        $requestHandler = (new MockRequestHandler())->setHandleExcaption(new \RuntimeException());
         $response = $catcher->process(
             new ServerRequest('GET', '/', ['Accept' => ['test/test']]),
-            $requestHandler
+            $this->getRequestHandler()
         );
         $response->getBody()->rewind();
         $content = $response->getBody()->getContents();
@@ -132,5 +162,10 @@ class ErrorCatcherTest extends TestCase
     private function getErrorCatcher(Container $container): ErrorCatcher
     {
         return new ErrorCatcher($this->getFactory(), $this->getErrorHandler(), $container);
+    }
+
+    private function getRequestHandler(): RequestHandlerInterface
+    {
+        return (new MockRequestHandler())->setHandleExcaption(new \RuntimeException());
     }
 }
