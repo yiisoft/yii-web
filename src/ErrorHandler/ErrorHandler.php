8854219663c4ea818e24061a7e673d27d0dc06b6
Alexander Makarov <sam@rmcreative.ru>
2020-08-17 12:37:39 +0300

    Error handler fixes (#303)
    * Fix rendering error during error rendering.
    * Fix incorrect order of arguments in some cases of HTML-rendered error page.
    * Do not use $_SERVER for rendering web server info, rely on PSR-7. Fix rendering itself.

--- src/ErrorHandler/ErrorHandler.php
+++ src/ErrorHandler/ErrorHandler.php
@@ -67,7 +67,7 @@ final class ErrorHandler
             $this->log($t);
             return $this->exposeDetails ? $renderer->renderVerbose($t) : $renderer->render($t);
         } catch (\Throwable $t) {
-            return nl2br((string)$t);
+            return (string)$t;
         }
     }
 
