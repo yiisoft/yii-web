63cf2647bc2c58e3c00a5aceeb562beb2523482d
Dmitriy Derepko <xepozz@list.ru>
2020-11-22 03:00:50 +0300

    Move out user and clean up dependencies

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -10,61 +10,15 @@ use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
-use Yiisoft\Auth\IdentityInterface;
 use Yiisoft\Yii\Web\Event\AfterEmit;
 use Yiisoft\Yii\Web\Event\AfterMiddleware;
 use Yiisoft\Yii\Web\Event\AfterRequest;
 use Yiisoft\Yii\Web\Event\BeforeMiddleware;
 use Yiisoft\Yii\Web\Event\BeforeRequest;
-use Yiisoft\Yii\Web\Tests\Mock\MockIdentity;
 use Yiisoft\Yii\Web\Tests\Mock\MockMiddleware;
-use Yiisoft\Yii\Web\User\Event\AfterLogin;
-use Yiisoft\Yii\Web\User\Event\AfterLogout;
-use Yiisoft\Yii\Web\User\Event\BeforeLogin;
-use Yiisoft\Yii\Web\User\Event\BeforeLogout;
 
 final class EventTest extends TestCase
 {
-    public function testUserAfterLoginEvent(): void
-    {
-        $event = new AfterLogin($this->createIdentity('test'));
-        $this->assertEquals('test', $event->getIdentity()->getId());
-    }
-
-    public function testUserAfterLogoutEvent(): void
-    {
-        $event = new AfterLogout($this->createIdentity('test'));
-        $this->assertEquals('test', $event->getIdentity()->getId());
-    }
-
-    public function testUserBeforeLogoutEvent(): void
-    {
-        $event = new BeforeLogout($this->createIdentity('test'));
-        $this->assertEquals('test', $event->getIdentity()->getId());
-        $this->assertTrue($event->isValid());
-    }
-
-    public function testUserBeforeLogoutEventInvalid(): void
-    {
-        $event = new BeforeLogout($this->createIdentity('test'));
-        $event->invalidate();
-        $this->assertFalse($event->isValid());
-    }
-
-    public function testUserBeforeLoginEvent(): void
-    {
-        $event = new BeforeLogin($this->createIdentity('test'));
-        $this->assertEquals('test', $event->getIdentity()->getId());
-        $this->assertTrue($event->isValid());
-    }
-
-    public function testUserBeforeLoginEventInvalid(): void
-    {
-        $event = new BeforeLogin($this->createIdentity('test'));
-        $event->invalidate();
-        $this->assertFalse($event->isValid());
-    }
-
     public function testAfterEmitEvent(): void
     {
         $event = new AfterEmit($this->createResponse(400));
@@ -110,11 +64,6 @@ final class EventTest extends TestCase
         $event = new BeforeMiddleware($middleware, $this->createRequest());
         $this->assertEquals('/test', $event->getRequest()->getUri());
         $this->assertEquals($middleware, $event->getMiddleware());
-    }
-
-    private function createIdentity(string $id): IdentityInterface
-    {
-        return new MockIdentity($id);
     }
 
     private function createResponse(int $code = 200): ResponseInterface
